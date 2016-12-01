2b3cf493675dfcbe483e620f58ed580c0ea0eb67
lmjing <lmjing@naver.com>
2016-12-01 23:24:28 +0900

    solve 1065

---
+++ backjoon/src/num2448.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by mijeong on 2016. 12. 1..
+ * 3*2^k     k<=10
+ */
+public class num2448 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int insert = stdin.nextInt();
+        //if(insert<)
+    }
+}
