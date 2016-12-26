082e42cd7e98e185d195126e62afc7ab24f11f8e
lmjing <lmjing@naver.com>
2016-12-26 17:53:53 +0900

    solve 7287

---
+++ backjoon/src/num7287.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mijeong on 2016. 12. 26..
+ */
+public class num7287 {
+    public static void main(String[] args){
+        System.out.println("39");
+        System.out.println("lmjing");
+    }
+}
