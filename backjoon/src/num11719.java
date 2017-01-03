253d30fb09a3199b73ed8728205127871084df2b
lmjing <lmjing@naver.com>
2017-01-03 13:01:08 +0900

    1/3

---
+++ backjoon/src/num11719.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Created by mijeong on 2017. 1. 2..
+ */
+public class num11719 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+
+        while(stdin.hasNextLine()){
+            String temp = stdin.nextLine();
+            System.out.println(temp);
+        }
+    }
+}
