bfccc6b23826a109dc7dbef006e5ee6946c366a8
lmjing <lmjing@naver.com>
2017-01-07 16:46:12 +0900

    slove 1110

---
+++ backjoon/src/num1110.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Created by mijeong on 2017. 1. 7..
+ */
+public class num1110 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int input = stdin.nextInt();
+        int temp = input;
+        int cnt = 0;
+
+        while (true) {
+            if (temp < 100 && temp >= 0) {
+                int x = temp / 10;
+                int y = temp % 10;
+                int z = (x+y) % 10;
+                temp = y * 10 + z;
+                cnt++;
+
+                if(input==temp) break;
+            }else break;
+        }
+
+        System.out.print(cnt);
+    }
+}
