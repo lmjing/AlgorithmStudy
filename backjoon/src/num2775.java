b2226fcc3bc2d307472ea741fd4a908834dd898f
lmjing <lmjing@naver.com>
2017-01-15 21:18:01 +0900

    solve 2775

---
+++ backjoon/src/num2775.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Created by mijeong on 2017. 1. 15..
+ */
+public class num2775 {
+    public static void main(String[] args){
+        Scanner stdin = new Scanner(System.in);
+        int cnt = stdin.nextInt();
+        int aa[][] = new int[15][15];
+        int result[] = new int[cnt];
+
+        for(int i=0; i<15; i++)
+            aa[0][i] = i+1;
+
+        for(int i=1;i<15;i++){
+            aa[i][0] = 1;
+            for(int j=1;j<15;j++){
+                aa[i][j] = aa[i][j-1] + aa[i-1][j];
+            }
+        }
+
+        for(int i=0;i<cnt;i++){
+            int k = stdin.nextInt();
+            int n = stdin.nextInt();
+            result[i] = aa[k][n-1];
+        }
+        for (int i=0;i<cnt;i++) System.out.println(result[i]);
+    }
+}
