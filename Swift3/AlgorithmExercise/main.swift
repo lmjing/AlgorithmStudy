84a7ee0e12965efdce235913d558db078e6f95ff
lmjing <lmjing1234@gmail.com>
2017-10-30 11:05:29 +0900

    num 11866번 해결

--- Swift3/AlgorithmExercise/main.swift
+++ Swift3/AlgorithmExercise/main.swift
@@ -15,3 +15,32 @@ import Foundation
 //woowaTest.testSolution1()
 //woowaTest.testSolution2()
 //woowaTest.testSolution3()
+
+let input = readLine()!
+
+let arr = input.characters.split(separator: " ").map{ Int(String($0))! }
+
+let m = arr[0]
+var n = arr[1]
+
+var array: [Int] = []
+
+for i in 1...m {
+    array.append(i)
+}
+
+var find = n
+var i = find - 1
+var result = "<"
+while array.count > 1 {
+    let count = array.count
+    if i >= count {
+        i %= count
+    }
+    result += "\(array[i]), "
+    array.remove(at: i)
+    
+    i += find - 1
+}
+result += "\(array.first!)>"
+print(result)
