dbec9c407332b27e5df4e52d78dc07b7217c567a
lmjing <lmjing1234@gmail.com>
2017-10-31 15:51:52 +0900

    2780 푸는 중

--- Swift3/AlgorithmExercise/main.swift
+++ Swift3/AlgorithmExercise/main.swift
@@ -16,3 +16,28 @@ import Foundation
 //woowaTest.testSolution2()
 //woowaTest.testSolution3()
 
+//var dp: [[[Int]]] = []
+//
+//dp.append([[1, 1, 1], [1, 1, 1], [1, 1, 1], [1]])
+//dp.append([[2, 3, 2], [3, 4, 3], [3, 3, 2], [1]])
+//
+//print(dp.count)
+//
+//func rect(n: Int, x: Int, y: Int) -> Int {
+//    return 1
+//}
+//
+//let count = Int(readLine()!)!
+//var answer: [Int] = []
+//for _ in 0..<count {
+//    let input = Int(readLine()!)!
+//    var sum = 0
+//    for i in 0..<4 {
+//        let y = i == 3 ? 1 : 3
+//        for j in 0..<y {
+//            sum += rect(n: input, x: i, y: j)
+//        }
+//    }
+//    answer.append(sum)
+//}
+//
