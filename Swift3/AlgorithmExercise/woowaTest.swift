2665f2430fb9394469947e381a75b6a55634c1e5
lmjing <lmjing1234@gmail.com>
2017-11-05 21:08:53 +0900

    gitignore 수정

--- Swift3/AlgorithmExercise/woowaTest.swift
+++ Swift3/AlgorithmExercise/woowaTest.swift
@@ -12,14 +12,6 @@ class WoowaTest {
     let test = Test()
     let algorithm = WoowaAlgorithm()
     
-//    func testSolution1() {
-//        let funcName = "woowa Solution1"
-//        var a = [3, 4, 4, 6, 1, 4, 4]
-////        test.equal(answer: algorithm.solution(5, &a), expect: [3, 2, 2, 4, 2], funcName: funcName)
-//        var a2 = [4, 4, 4, 4, 4, 4, 4, 4, 4, 6, 1, 6]
-//        test.equal(answer: algorithm.solution(5, &a2), expect: [10, 10, 10, 10, 10], funcName: funcName)
-//    }
-    
     func testSolution1() {
         let funcName = "woowa Solution1"
         var temp = "(())"
