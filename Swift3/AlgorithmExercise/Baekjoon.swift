7342a53b1ab52b2745a2cd90d088a632a6a289ac
lmjing <lmjing1234@gmail.com>
2017-11-12 19:31:34 +0900

    2875 품

--- Swift3/AlgorithmExercise/Baekjoon.swift
+++ Swift3/AlgorithmExercise/Baekjoon.swift
@@ -737,4 +737,15 @@ class Baekjun {
         print(dfsResult)
         print(bfsResult)
     }
+    
+    func num2875() {
+        let input = readLine()!.split(separator: " ").map{ Int($0)! }
+        let n = input[0]; let m = input[1]; let k = input[2]
+        
+        var min = n / 2 < m ? n / 2 : m
+        while n + m - min * 3 < k {
+            min -= 1
+        }
+        print(min)
+    }
 }
--- Swift3/AlgorithmExercise/main.swift
+++ Swift3/AlgorithmExercise/main.swift
@@ -16,3 +16,13 @@ import Foundation
 //woowaTest.testSolution2()
 //woowaTest.testSolution3()
 
+Baekjun().num2875()
+
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+let n = input[0]; let m = input[1]; let k = input[2]
+
+var min = n / 2 < m ? n / 2 : m
+while n + m - min * 3 < k {
+    min -= 1
+}
+print(min)
