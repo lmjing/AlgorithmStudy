65f030cd5aee0a1cfd435cb04ea84ee5a69346f3
lmjing <lmjing1234@gmail.com>
2017-11-13 17:04:41 +0900

    6064번 풀었으나 시간초과

--- Swift3/AlgorithmExercise/main.swift
+++ Swift3/AlgorithmExercise/main.swift
@@ -16,13 +16,5 @@ import Foundation
 //woowaTest.testSolution2()
 //woowaTest.testSolution3()
 
-Baekjun().num2875()
+Baekjun().num6064()
 
-let input = readLine()!.split(separator: " ").map{ Int($0)! }
-let n = input[0]; let m = input[1]; let k = input[2]
-
-var min = n / 2 < m ? n / 2 : m
-while n + m - min * 3 < k {
-    min -= 1
-}
-print(min)
