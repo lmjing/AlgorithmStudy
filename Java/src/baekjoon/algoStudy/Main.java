4d09a0c7e193f97c9836d43d7fba20cb326b6d6a
lmjing <lmjing1234@gmail.com>
2018-01-03 23:10:50 +0900

    2439번 문제 해결

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -7,7 +7,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j <= i; j++) {
+            for(int j = n-1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for(int k = 0; k <= i; k++) {
                 System.out.print("*");
             }
             System.out.println("");
--- Java/src/baekjoon/algoStudy/chapter1_input_output/StarPrint.java
+++ Java/src/baekjoon/algoStudy/chapter1_input_output/StarPrint.java
@@ -13,4 +13,18 @@ public class StarPrint {
             System.out.println("");
         }
     }
+
+    public void num2439() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            for(int j = n-1; j > i; j--) {
+                System.out.print(" ");
+            }
+            for(int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
