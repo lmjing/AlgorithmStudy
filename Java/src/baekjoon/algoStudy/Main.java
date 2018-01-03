8462029eb5cd7f1e5d21b07496786e50a0b86e1e
lmjing <lmjing1234@gmail.com>
2018-01-03 23:58:24 +0900

    2522번 문제 해결

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i=0; i<n*2-1; i++) {
-            int temp = i<n? i : n*2-2-i;
-            for(int j=0; j<n*2-temp-1; j++) {
-                if(j>=temp) System.out.print("*");
-                else System.out.print(" ");
+        for (int i=1; i<n*2; i++) {
+            for(int j=1; j<=n; j++) {
+                int temp = i<=n ? i : n*2-i;
+                if(j <= n-temp) System.out.print(" ");
+                else System.out.print("*");
             }
             System.out.println("");
         }
--- Java/src/baekjoon/algoStudy/chapter1_input_output/StarPrint.java
+++ Java/src/baekjoon/algoStudy/chapter1_input_output/StarPrint.java
@@ -116,4 +116,17 @@ public class StarPrint {
             System.out.println("");
         }
     }
+
+    public void num2522() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i=1; i<n*2; i++) {
+            for(int j=1; j<=n; j++) {
+                int temp = i<=n ? i : n*2-i;
+                if(j <= n-temp) System.out.print(" ");
+                else System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
