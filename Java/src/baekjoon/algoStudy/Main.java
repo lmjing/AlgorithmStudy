973e4b4c865e28b9fe36da402819147a7e0d3bca
lmjing <lmjing1234@gmail.com>
2018-01-03 22:23:59 +0900

    11720번 문제 해결

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,5 +3,6 @@ package baekjoon.algoStudy;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
     }
 }
--- Java/src/baekjoon/algoStudy/chapter1_input_output/InputOuput.java
+++ Java/src/baekjoon/algoStudy/chapter1_input_output/InputOuput.java
@@ -15,4 +15,16 @@ public class InputOuput {
             System.out.println(sc.nextLine());
         }
     }
+
+    public void num11720() {
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        int sum = 0;
+        String[] input = sc.nextLine().split("");
+
+        for(int i=0; i<N; i++) {
+            sum += Integer.parseInt(input[i]);
+        }
+        System.out.println(sum);
+    }
 }
