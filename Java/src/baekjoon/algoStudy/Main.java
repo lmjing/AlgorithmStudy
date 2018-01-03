64b4b3cd90cb16f43b8fb63acbcbedbfddaeb289
lmjing <lmjing1234@gmail.com>
2018-01-03 21:52:11 +0900

    10952번 문제 해결

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -1,12 +1,9 @@
 package baekjoon.algoStudy;
 
-import baekjoon.algoStudy.chapter1_input_output.AddAB;
-
-import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        AddAB addAB = new AddAB();
-        addAB.num10951();
+    public static void main(String[] args) {
+
     }
 }
--- Java/src/baekjoon/algoStudy/chapter1_input_output/AddAB.java
+++ Java/src/baekjoon/algoStudy/chapter1_input_output/AddAB.java
@@ -60,4 +60,14 @@ public class AddAB {
             System.out.println(a + b);
         }
     }
+
+    public void num10952() {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a == 0 && b == 0) break;
+            System.out.println(a + b);
+        }
+    }
 }
