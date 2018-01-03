246a20a6ac0545d5eaeae84f6268a92d5128f33e
lmjing <lmjing1234@gmail.com>
2018-01-03 23:11:58 +0900

    2440번 문제 해결

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -7,10 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 0; i < n; i++) {
-            for(int j = n-1; j > i; j--) {
-                System.out.print(" ");
-            }
-            for(int k = 0; k <= i; k++) {
+            for(int j = n; j > i; j--) {
                 System.out.print("*");
             }
             System.out.println("");
--- Java/src/baekjoon/algoStudy/chapter1_input_output/StarPrint.java
+++ Java/src/baekjoon/algoStudy/chapter1_input_output/StarPrint.java
@@ -27,4 +27,15 @@ public class StarPrint {
             System.out.println("");
         }
     }
+
+    public void num2440() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            for(int j = n; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
