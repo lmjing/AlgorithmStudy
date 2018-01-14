118651ca5a7f52bf5ccd6411c9fc3c6f1d1d8500
lmjing <lmjing1234@gmail.com>
2018-01-15 02:01:56 +0900

    num1963 문제 해결

--- Java/src/baekjoon/algoStudy/chapter2_brute_force_search1/UsingQueue.java
+++ Java/src/baekjoon/algoStudy/chapter2_brute_force_search1/UsingQueue.java
@@ -1,5 +1,6 @@
 package baekjoon.algoStudy.chapter2_brute_force_search1;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -36,6 +37,70 @@ public class UsingQueue {
         Node(int index, int depth) {
             this.index = index;
             this.depth = depth;
+        }
+    }
+
+    public static void num1963() {
+        //1. 일단 네 자리 소수를 다 만든다. (1000 이상) (소수의 여부를 확인하는 배열을 만들 것)
+        ArrayList<Integer> four_majority = new ArrayList<Integer>();
+        makeMajority(four_majority);
+
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i=0; i<n; i++) {
+            int start = sc.nextInt(); int end = sc.nextInt();
+
+            LinkedList<MajorityPW> queue = new LinkedList<MajorityPW>();
+            queue.add(new MajorityPW(start, 0));
+            int[] visited = new int[10000];
+
+            while (!queue.isEmpty() && queue.getFirst().password != end) {
+                MajorityPW current = queue.removeFirst();
+                visited[current.password] = 1;
+                for (int majority : four_majority) {
+                    if (visited[majority] == 0 && check(majority, current.password)) {
+                        queue.addLast(new MajorityPW(majority, current.depth + 1));
+                    }
+                }
+            }
+            sc.nextLine();
+            System.out.println(queue.isEmpty() ? "Impossible" : queue.getFirst().depth);
+        }
+    }
+
+    private static boolean check(int majority, int su) {
+        int count = 0;
+        while (su > 0 && majority > 0) {
+            if (su % 10 != majority % 10) count++;
+            su /= 10; majority /= 10;
+        }
+        return count == 1 ? true : false;
+    }
+
+    private static class MajorityPW {
+        int depth;
+        int password;
+
+        MajorityPW(int password, int depth) {
+            this.password = password;
+            this.depth = depth;
+        }
+    }
+
+    private static void makeMajority(ArrayList<Integer> four_majority) {
+        ArrayList<Integer> majority = new ArrayList<Integer>();
+        for (int i=2; i<10000; i++) {
+            boolean flag = true;
+            for (int ma: majority) {
+                if (i % ma == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                majority.add(i);
+                if (i >= 1000) four_majority.add(i);
+            }
         }
     }
 }
