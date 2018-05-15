580c70edb24c1b2d5169789d52c46a8f706dd75e
lmjing <lmjing1234@gmail.com>
2018-05-15 19:13:14 +0900

    1208 2차 시도 -> 테스트케이스 잘 되는거 같은데 제출하니깐 첨부터 틀렸다고 뜸

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -7,7 +7,6 @@ public class Main {
     static int n;
     static int s;
     static int[] array;
-    static int count = 0;
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
@@ -17,18 +16,58 @@ public class Main {
         for (int i=0; i<n; i++) {
             array[i] = sc.nextInt();
         }
-        nextSubset(0, 1, array[0]);
-        nextSubset(0, 0, 0);
-        System.out.println(count);
+        Subset subset = new Subset();
+        System.out.println(subset.getAnswer());
     }
 
-    static void nextSubset (int i, int c, int sum) {
-        if (n <= i) {
-            if (sum == s && c > 0)
-                count++;
-            return;
+    static class Subset {
+        long[] frontHalf;
+        long[] afterHalf;
+        int frontArrow = 0;
+        int afterArrow = 0;
+        int idx = 0;
+
+        public Subset() {
+            int half = n / 2;
+            frontHalf = new long[(int) Math.pow(2, half)];
+            afterHalf = new long[(int) Math.pow(2, n - half)];
+
+            makeSubset(0, half, 0,true);
+            idx = 0;
+            makeSubset(half, n, 0, false);
+            Arrays.sort(frontHalf);
+            Arrays.sort(afterHalf);
+            afterArrow = afterHalf.length-1;
         }
-        nextSubset(i+1, c+1, sum + array[i]);
-        nextSubset(i+1, c, sum);
+
+        public int getAnswer() {
+            int count = 0;
+            while (frontArrow < frontHalf.length && afterArrow >= 0) {
+                long a = frontHalf[frontArrow];
+                long b = afterHalf[afterArrow];
+                long sum = a + b;
+                if(sum == s && !(a == 0 && b == 0))  {
+                    int i1 = 1; int i2 = 1;
+                    while (++frontArrow < frontHalf.length && frontHalf[frontArrow] == a) i1++;
+                    while (--afterArrow >= 0 && afterHalf[afterArrow] == b) i2++;
+                    count += i1 * i2;
+                }else {
+                    if (sum >= 0) afterArrow--;
+                    else frontArrow++;
+                }
+            }
+            return count;
+        }
+
+        private void makeSubset(int ai, int e, int sum, boolean flag) {
+            long[] halfArray = flag ? frontHalf : afterHalf;
+
+            if(ai == e) {
+                halfArray[idx++] = sum;
+                return;
+            }
+            makeSubset(ai + 1, e, sum, flag);
+            makeSubset(ai + 1, e, sum + array[ai], flag);
+        }
     }
 }
