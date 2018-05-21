50aa8af765b5bf6535e0f7ff212860cde4d1590a
lmjing <lmjing1234@gmail.com>
2018-05-21 17:11:01 +0900

    7453번 7차시도=정답 - 전역 변수 지역 변수로 변경 더 빨라짐 + 함수 따로 뺌

--- Java/src/baekjoon/algoStudy/chapter2_brute_force_search2/TwoPointerAlgorithms.java
+++ Java/src/baekjoon/algoStudy/chapter2_brute_force_search2/TwoPointerAlgorithms.java
@@ -1,6 +1,7 @@
 package baekjoon.algoStudy.chapter2_brute_force_search2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TwoPointerAlgorithms {
@@ -96,5 +97,54 @@ public class TwoPointerAlgorithms {
             }
         }
         return prime;
+    }
+
+    public static void num7453() {
+        Scanner sc = new Scanner(System.in);
+        int[][] input;
+        int[] index = {0, 0, 0, 0};
+        long count = 0;
+        int n;
+
+        n = sc.nextInt();
+        input = new int[n][4];
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<4; j++) {
+                input[i][j]= sc.nextInt();
+            }
+        }
+
+        int N = n*n;
+        long[] front = new long[N];
+        long[] back = new long[N];
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                front[i * n + j] = input[i][0] + input[j][1];
+                back[i * n + j] = input[i][2] + input[j][3];
+            }
+        }
+
+        Arrays.sort(front);
+        Arrays.sort(back);
+
+        int fi = 0;
+        int bi = N - 1;
+
+        while (fi < N && bi >= 0) {
+            long f = front[fi]; long b = back[bi];
+            if (f * b == 1) break;
+
+            long sum = f + b;
+            if (sum == 0) {
+                long c1 = 1, c2 = 1;
+                while (++fi < N && front[fi] == f) c1++;
+                while (--bi >= 0 && back[bi] == b) c2++;
+                count += c1 * c2;
+            }else if (sum > 0) bi--;
+            else fi++;
+        }
+
+        System.out.println(count);
     }
 }
