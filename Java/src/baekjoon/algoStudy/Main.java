880af39dd269b1091a9c55e975f39d1d2c0b52d2
lmjing <lmjing1234@gmail.com>
2018-05-24 19:34:18 +0900

    num2143 4차시도 - ArrayList -> 배열 변경  => 68%쯤 틀렸습니다.

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -13,21 +13,20 @@ public class Main {
         Array secondArray = new Array(sc.nextInt());
         secondArray.setInitArray();
 
-        Collections.sort(firstArray.subSumArray);
-        Collections.sort(secondArray.subSumArray);
-        int fi = 0; int si = secondArray.subSumArray.size() - 1;
+        Arrays.sort(firstArray.subSumArray);
+        Arrays.sort(secondArray.subSumArray);
+        int fi = 0; int si = secondArray.last - 1;
         int result = 0;
 
-        int firstSize = firstArray.subSumArray.size();
-        while (fi < firstSize && si >= 0) {
-            int f = firstArray.subSumArray.get(fi);
-            int s = secondArray.subSumArray.get(si);
+        while (fi < firstArray.last && si >= 0) {
+            int f = firstArray.subSumArray[fi];
+            int s = secondArray.subSumArray[si];
             int sum = f + s;
 
             if (sum == T) {
                 int cnt1 = 1; int cnt2 = 1;
-                while (++fi < firstSize && firstArray.subSumArray.get(fi) == f) cnt1++;
-                while (--si >= 0 && secondArray.subSumArray.get(si) == s) cnt2++;
+                while (++fi < firstArray.last && firstArray.subSumArray[fi] == f) cnt1++;
+                while (--si >= 0 && secondArray.subSumArray[si] == s) cnt2++;
                 result += cnt1 * cnt2;
             }else if (sum > T) --si;
             else ++fi;
@@ -38,15 +37,17 @@ public class Main {
 
     static class Array {
         int n;
-        List<Integer> subSumArray = new LinkedList<>();
+        int last = 0;
+        int[] subSumArray;
         int[] sumTempArray;
-//        SortedMap<Integer, Integer> countMap = new TreeMap<>();
-//        int[][] subArray;
 
         public Array(int n) {
             this.n = n;
             sumTempArray = new int[n + 1];
-//            subArray = new int[n][n + 1];
+            int size = (n/2) * (1 + n);
+            if (n % 2 == 1) size += n/2 + 1;
+
+            subSumArray = new int[size];
         }
 
         public void setInitArray() {
@@ -55,7 +56,7 @@ public class Main {
                 for (int j=i; j>0; j--) {
                     int sum = sumTempArray[j-1] + num;
                     sumTempArray[j] = sum;
-                    subSumArray.add(sum);
+                    subSumArray[last++] = sum;
                 }
             }
         }
