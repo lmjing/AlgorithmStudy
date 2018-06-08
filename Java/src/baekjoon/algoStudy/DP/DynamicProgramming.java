9ba9aa83c3de4f4984c569ddb6ee1d377dde29a3
lmjing <lmjing1234@gmail.com>
2018-06-08 13:46:41 +0900

    1463 4차시도(다른 방식) 맞음

---
+++ Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
@@ -0,0 +1,42 @@
+package baekjoon.algoStudy.DP;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class DynamicProgramming {
+    static class num1463 {
+        static Scanner sc;
+        static boolean[] array;
+        public static void main (String[] args) {
+            sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            Queue<Integer> before = new LinkedList<>();
+            Queue<Integer> after = new LinkedList<>();
+            array = new boolean[n+1];
+            int count = 0;
+            before.add(n);
+
+            while (!before.isEmpty()) {
+                int num = before.remove();
+                if (num == 1) {
+                    System.out.println(count);
+                    break;
+                }
+                if (num > 1 && !array[num]) {
+                    array[num] = true;
+                    if (num % 3 == 0) after.add(num / 3);
+                    if (num % 2 == 0) after.add(num / 2);
+                    after.add(num - 1);
+                }
+
+                if (before.isEmpty()) {
+                    before = after;
+                    after = new LinkedList<>();
+                    count++;
+                }
+            }
+        }
+    }
+}
