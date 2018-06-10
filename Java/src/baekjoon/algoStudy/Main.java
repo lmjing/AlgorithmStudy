4b692e49abbabd9b7d5a38fc0278aaf937ec5223
lmjing <lmjing1234@gmail.com>
2018-06-10 16:02:02 +0900

    num1463 bottom-up 방식 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,17 @@ import java.util.*;
 
 public class Main {
     public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] minArray = new int[n+1];
+
+        for (int i = 2; i <= n; i++) {
+            int min = minArray[i-1] + 1;
+            if (i % 3 == 0) min = Math.min(min, minArray[i/3] + 1);
+            if (i % 2 == 0) min = Math.min(min, minArray[i/2] + 1);
+            minArray[i] = min;
+        }
+        System.out.println(minArray[n]);
     }
 }
