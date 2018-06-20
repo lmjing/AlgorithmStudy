7e4529217c7f3db0be5ef95725907339c9d950dc
lmjing <lmjing1234@gmail.com>
2018-06-20 21:26:13 +0900

    11726번 top-down 옮김

--- Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
+++ Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
@@ -57,5 +57,28 @@ public class DynamicProgramming {
 
             System.out.println(tile[n]);
         }
+
+        public static void top_down () {
+            Scanner sc  = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            tile = new long[n+1];
+
+            tile[1] = 1;
+            if (n > 1) tile[2] = 2;
+
+            System.out.println(goTo(n));
+        }
+
+        public static long goTo (int n) {
+            if (n==1) return 1;
+            if (n==2) return 2;
+
+            long a = tile[n-1] > 0 ? tile[n-1] : goTo(n-1);
+            long b = tile[n-2] > 0 ? tile[n-2] : goTo(n-2);
+            tile[n] = (a + b) % 10007;
+
+            return tile[n];
+        }
     }
 }
--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,25 +5,5 @@ import java.util.*;
 public class Main {
     static long[] tile;
     public static void main (String[] args) {
-        Scanner sc  = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        tile = new long[n+1];
-
-        tile[1] = 1;
-        if (n > 1) tile[2] = 2;
-
-        System.out.println(goTo(n));
-    }
-
-    public static long goTo (int n) {
-        if (n==1) return 1;
-        if (n==2) return 2;
-
-        long a = tile[n-1] > 0 ? tile[n-1] : goTo(n-1);
-        long b = tile[n-2] > 0 ? tile[n-2] : goTo(n-2);
-        tile[n] = (a + b) % 10007;
-
-        return tile[n];
     }
 }
