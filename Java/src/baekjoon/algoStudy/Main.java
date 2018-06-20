0aff4939119903e756890752e19f32ff4dd42139
lmjing <lmjing1234@gmail.com>
2018-06-20 19:39:55 +0900

    11726번 1차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -1,6 +1,29 @@
 package baekjoon.algoStudy;
 
+import java.util.*;
+
 public class Main {
+    static long[] ans;
     public static void main (String[] args) {
+        Scanner sc  = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ans = new long[n+1];
+
+        ans[1] = 1;
+        if (n > 1) ans[2] = 2;
+
+        System.out.println(get(n));
+    }
+
+    public static long get(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        long a = ans[n-1] > 0 ? ans[n-1] : get(n-1);
+        long b = ans[n-2] > 0 ? ans[n-2] : get(n-2);
+
+        ans[n] = a + b;
+        return a + b;
     }
 }
