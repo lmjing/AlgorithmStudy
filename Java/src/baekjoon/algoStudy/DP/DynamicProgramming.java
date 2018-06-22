f277dea92f5d04ab38883ca6a09624f9e6a03935
lmjing <lmjing1234@gmail.com>
2018-06-22 11:42:08 +0900

    11727번 1차시도 bottom-up 성공

--- Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
+++ Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
@@ -81,4 +81,22 @@ public class DynamicProgramming {
             return tile[n];
         }
     }
+
+    static class Num11727 {
+        static int[] tile;
+        public static void bottom_up () {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            tile = new int[n+1];
+            tile[1] = 1;
+            if (n > 1) tile[2] = 3;
+
+            for (int i = 3; i <= n; i++) {
+                tile[i] = (tile[i-2] * 2 + tile[i-1]) % 10007;
+            }
+
+            System.out.println(tile[n]);
+        }
+    }
 }
--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,7 +3,19 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
-    static long[] tile;
+    static int[] tile;
     public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        tile = new int[n+1];
+        tile[1] = 1;
+        if (n > 1) tile[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            tile[i] = (tile[i-2] * 2 + tile[i-1]) % 10007;
+        }
+
+        System.out.println(tile[n]);
     }
 }
