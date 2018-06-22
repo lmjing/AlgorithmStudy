8e498c035c5be8fae283fba5ec85ead74ee7ed97
lmjing <lmjing1234@gmail.com>
2018-06-22 11:54:54 +0900

    main 정리

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,21 +3,6 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
-    static int[] tile;
     public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        tile = new int[n+1];
-        tile[1] = 1;
-        if (n > 1) tile[2] = 3;
-
-        System.out.println(goTo(n));
-    }
-
-    public static int goTo (int n) {
-        if (n > 2)
-            tile[n] = (goTo(n-1) + tile[n-2] * 2) % 10007;
-        return tile[n];
     }
 }
