344f79fe6fa29391da4f9a15d41ee972e2bbbf46
lmjing <lmjing1234@gmail.com>
2018-06-25 23:55:32 +0900

    9095번 bottom-top 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,6 +3,23 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
+    static int[] answer = new int[11];
     public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        answer[1] = 1;
+        answer[2] = 1;
+        answer[3] = 1;
+        for (int i = 2; i <= 10; i++) {
+            for (int j = 1; j <= 3 && j < i; j++) {
+                answer[i] += answer[i - j];
+            }
+        }
+
+        for (int i = 0; i < T; i++) {
+            int n = sc.nextInt();
+            System.out.println(answer[n]);
+        }
     }
 }
