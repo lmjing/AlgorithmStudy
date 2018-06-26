a7f9e4ec8c26d1f82e025ad480af9e60f45f3311
lmjing <lmjing1234@gmail.com>
2018-06-26 16:33:35 +0900

    11052번 bottom-up 1차 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,6 +3,25 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
-    public static void main (String[] args) {
+    static int[] max;
+    static int[] price;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        price = new int[n + 1];
+        max = new int[n + 1];
+        max[1] = price[1];
+
+        for (int i = 1; i <= n; i++) {
+            price[i] = sc.nextInt();
+            max[i] = price[i];
+            for (int j = i - 1; j >= i / 2 && i > 1; j--) {
+                if (max[j] + price[i - j] > max[i]) max[i] = max[j] + price[i - j];
+            }
+        }
+
+        System.out.println(max[n]);
     }
 }
