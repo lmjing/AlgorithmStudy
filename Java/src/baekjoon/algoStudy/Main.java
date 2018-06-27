d7dbf0926857ba3073c8b3403c0176786007ff32
lmjing <lmjing1234@gmail.com>
2018-06-27 23:46:09 +0900

    10844번 1차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] answer = new int[n+1];
+        int[] temp = new int[n+1];
+
+        answer[1] = 9;
+        temp[1] = 1;
+
+        for (int i=2; i<=n; i++) {
+            answer[i] = answer[i-1]*2-temp[i-1];
+        }
+
+        System.out.println(answer[n]);
     }
 }
