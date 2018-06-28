e4b0d5ae5caea4a74b3353aea8b973555e026738
lmjing <lmjing1234@gmail.com>
2018-06-28 23:56:54 +0900

    코드 이동

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,23 +4,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        long sum = 0;
-        for (int su : goTo(n)) sum += su;
-        System.out.println(sum % 10007);
-    }
-
-    static int[] goTo (int n) {
-        int[] current = new int[10];
-        if (n == 1) {
-            for (int i = 0; i < 10; i++) current[i] = 1;
-        }else {
-            int[] before = goTo(n - 1);
-            current[0] = 1;
-            for (int i = 1; i < 10; i++) current[i] = (current[i-1] + before[i]) % 10007;
-        }
-        return current;
+        
     }
 }
