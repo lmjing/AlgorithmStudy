13b413ea95a76d729fe39c4d10cdfd09ee52ece9
lmjing <lmjing1234@gmail.com>
2018-07-01 15:39:02 +0900

    10844 같은 원리 코드 5차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -7,23 +7,37 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        long[] count = new long[10];
-        long sum = 9;
-        for (int i = 1; i < 10; i++) count[i] = 1;
+        long[][] dp = new long[N][10];
+
+        for (int i = 1; i < 10; i++) dp[1][i] = 1;
 
         for (int i = 2; i < N; i++) {
-            long[] temp = new long[10];
-            temp[0] = count[1] % 10007;
-            temp[9] = count[8] % 10007;
-            sum = temp[0] + temp[9];
-
-            for (int j = 1; j < 9; j++) {
-                temp[j] = (count[j-1] + count[j+1]) % 1000000000;
-                sum += temp[j];
-            }
-            count = temp;
+            dp[i][0] = dp[i-1][1];
+            dp[i][9] = dp[i-1][8];
+            for (int j = 1; j < 9; j++) dp[i][j] = dp[i-1][j+1] + dp[i-1][j-1];
         }
 
-        System.out.println(N == 1 ? sum : (sum * 2 - count[0] - count[9]) % 1000000000);
+        long sum = 0;
+        for (long su : dp[N-1]) sum += su;
+        System.out.println((sum * 2 - (dp[N-1][0] + dp[N-1][9])) % 1000000000);
+
+//        long[] count = new long[10];
+//        long sum = 9;
+//        for (int i = 1; i < 10; i++) count[i] = 1;
+//
+//        for (int i = 2; i < N; i++) {
+//            long[] temp = new long[10];
+//            temp[0] = count[1] % 10007;
+//            temp[9] = count[8] % 10007;
+//            sum = temp[0] + temp[9];
+//
+//            for (int j = 1; j < 9; j++) {
+//                temp[j] = (count[j-1] + count[j+1]) % 1000000000;
+//                sum += temp[j];
+//            }
+//            count = temp;
+//        }
+//
+//        System.out.println(N == 1 ? sum : (sum * 2 - count[0] - count[9]) % 1000000000);
     }
 }
