91f12d4eb5d60a3c62676c6a65af66f4ae8505f1
lmjing <lmjing1234@gmail.com>
2018-07-02 18:41:23 +0900

    2193번 3차시도 top_down 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -6,15 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long[][] dp = new long[n][3];
-        dp[0] = new long[]{1, 0, 1};
 
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = dp[i-1][1] * 2 + dp[i-1][2];
-            dp[i][1] = dp[i-1][1] + dp[i-1][2];
-            dp[i][2] = dp[i-1][1];
-        }
+        System.out.println(goTo(n)[0]);
+    }
 
-        System.out.println(dp[n-1][0]);
+    static long[] goTo(int n) {
+        if (n == 1) return new long[]{1, 0, 1};
+
+        long[] current = new long[3];
+        long[] before = goTo(n - 1);
+        current[0] = before[1] * 2 + before[2];
+        current[1] = before[1] + before[2];
+        current[2] = before[1];
+        return current;
     }
 }
