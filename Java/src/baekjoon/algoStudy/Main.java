47f52be7ea1f4d24c05190d7b672a804c03ae7b0
lmjing <lmjing1234@gmail.com>
2018-07-03 22:43:35 +0900

    2156 4차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,39 +3,23 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
-    static long[][] count;
-    static long max = 0;
-    static int N;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        int size = (N + 2) / 3 * 3;
-        int[] input = new int[size];
-        count = new long[N/3 + 1][3];
+        int n = sc.nextInt();
+        int[] input = new int[n];
+        long[][] dp = new long[n][2];
 
-        long sum = 0;
-        for (int i = 0; i < size; i++) {
-            if (i < N)
-                input[i] = sc.nextInt();
-            sum += input[i];
-            if (i % 3 == 2) {
-                count[i/3][0] = sum - input[i - 2];
-                count[i/3][1] = sum - input[i - 1];
-                count[i/3][2] = sum - input[i];
-                sum = 0;
+        for (int i = 0; i < n; i++) {
+            input[i] = sc.nextInt();
+            if (i == 0) dp[0][1] = input[i];
+            else if (i == 1) {
+                dp[1][0] = input[i-1];
+                dp[1][1] = input[i-1] + input[i];
+            } else {
+              dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+              dp[i][1] = input[i] + Math.max(dp[i-2][0] + input[i-1], dp[i-1][0]);
             }
         }
-        goTo(0, 3, 0);
-        System.out.println(max);
-    }
-
-    public static void goTo (int n, int before, long sum) {
-        if (n == count.length) {
-            max = max < sum ? sum : max;
-            return;
-        }
-        if (before >= 2) goTo(n + 1, 2, sum + count[n][2]);
-        if (before >= 1) goTo(n + 1, 1, sum + count[n][1]);
-        if (before >= 0) goTo(n + 1, 0, sum + count[n][0]);
+        System.out.println(Math.max(dp[n-1][0], dp[n-1][1]));
     }
 }
