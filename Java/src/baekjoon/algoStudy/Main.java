ee184dfafbbf2e2cf5ddddb44bf06ad11d3859f9
lmjing <lmjing1234@gmail.com>
2018-07-04 17:44:46 +0900

    11053 코드 짜는 중, 자료구조에 어려움 겪고 있음

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -1,8 +1,34 @@
 package baekjoon.algoStudy;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] input = new int[n]; // 0 : value, 1 : count
+//        int[] sorted = new int[n];
+        LinkedList<Integer> sorted = new LinkedList<>();
+
+        long[] count = new long[1000];
+
+        for (int i = 0; i < n; i++) {
+            input[i] = sc.nextInt();
+//            sorted[i] = input[i];
+            sorted.add(input[i]);
+            count[input[i]] = 1;
+        }
+//        Arrays.sort(sorted);
+        Collections.sort(sorted);
+
+        for (int i = 1; i < n; i++) {
+            if (input[i-1] < input[i]) {
+                count[i] += Math.max(count[input[i-1]], count[sorted.indexOf(input[i]) - 1]);
+            }else {
+                Iterator<Integer> t = sorted.iterator();
+                
+            }
+        }
     }
 }
