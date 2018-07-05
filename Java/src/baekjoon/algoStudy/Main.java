c5d3032a10b224e9977c981d80d1b03b10da26d0
lmjing <lmjing1234@gmail.com>
2018-07-05 19:16:01 +0900

    11053 답 참고 3차시도 내 방식대로 짜보는데 계속 틀림

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -1,24 +1,50 @@
 package baekjoon.algoStudy;
-
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] dp = new int[n][2]; // 0 : length, 1 : last
-        int max = 1;
+        int[] input = new int[n];
+        int[] count = new int[1001];
 
         for (int i = 0; i < n; i++) {
-            dp[i][0] = 1;
-            dp[i][1] = sc.nextInt();
-            for (int j = 0; j < i; j++) {
-                if (dp[j][1] < dp[i][1])
-                    dp[i][0] = Math.max(dp[i][0], dp[j][0] + 1);
-            }
-            max = max < dp[i][0] ? dp[i][0] : max;
+            input[i] = sc.nextInt();
         }
-        System.out.println(max);
+
+        count[input[0]] = 1;
+        int[] max = { 1, 1 };
+
+        for (int i = 1; i < n; i++) {
+            int smallIdx = i - 1;
+            while (smallIdx >= 0 && input[smallIdx] >= input[i]) smallIdx--;
+            count[input[i]] = smallIdx >= 0 ? count[input[smallIdx]] + 1 : 1;
+
+            int v = input[i];
+            if (max[0] < v) count[v] = max[1] + 1;
+            else {
+                if (v > input[i - 1])
+                    count[v] = Math.max(count[v], count[input[i - 1]]);
+
+            }
+
+
+            if (max[0] < input[i]) count[input[i]] = max[1] + 1;
+            else if (max[0] > input[i]){
+                int smallIdx = i - 1;
+                while (smallIdx >= 0 && input[smallIdx] >= input[i]) smallIdx--;
+                count[input[i]] = smallIdx >= 0 ? count[input[smallIdx]] + 1 : 1;
+
+                if (input[i - 1] < input[i])
+                    count[input[i]] = Math.max(count[input[i]], count[input[i - 1]] + 1);
+            }
+
+            if (max[1] < count[input[i]]) {
+                max[0] = input[i];
+                max[1] = count[input[i]];
+            }
+        }
+        System.out.print(max[1]);
     }
 }
