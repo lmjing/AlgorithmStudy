6bd2a69065a062535451cbc19eea1a6a558460a7
lmjing <lmjing1234@gmail.com>
2018-07-05 17:09:00 +0900

    11053 답 참고 2차시도 bottom_up 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -1,40 +1,24 @@
 package baekjoon.algoStudy;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] input = new int[n];
-        int[] sorted = new int[n];
-        int[][] dp = new int[1000][2]; // 0 : count, 1 : idx
+        int[][] dp = new int[n][2]; // 0 : length, 1 : last
+        int max = 1;
 
         for (int i = 0; i < n; i++) {
-            input[i] = sc.nextInt();
-            sorted[i] = input[i];
-        }
-        Arrays.sort(sorted);
-        for (int i = 0; i < n; i++) dp[sorted[i]][1] = i;
-
-        dp[0][0] = 1;
-        int maxLength = 1;
-        for (int i = 1; i < n; i++) {
-            int v = input[i];
-            if (input[i-1] < v) {
-                dp[v][0] = Math.max(dp[v][0], dp[input[i-1]][0] + 1);
-                int maxSmallIdx = dp[v][1] - 1;
-                if (maxSmallIdx > -1) {
-                    dp[v][0] = Math.max(dp[v][0], dp[sorted[maxSmallIdx]][0] + 1);
-                }
-            } else if (input[i-1] > v && dp[v][0] == 0) {
-                int before = dp[v][1] - 1;
-                while (before >= 0 && dp[sorted[before]][0] == 0) --before;
-                dp[v][0] = before == -1 ? 1 : dp[sorted[before]][0] + 1;
+            dp[i][0] = 1;
+            dp[i][1] = sc.nextInt();
+            for (int j = 0; j < i; j++) {
+                if (dp[j][1] < dp[i][1])
+                    dp[i][0] = Math.max(dp[i][0], dp[j][0] + 1);
             }
-            maxLength = maxLength < dp[v][0] ? dp[v][0] : maxLength;
+            max = max < dp[i][0] ? dp[i][0] : max;
         }
-
-        System.out.println(maxLength);
+        System.out.println(max);
     }
 }
