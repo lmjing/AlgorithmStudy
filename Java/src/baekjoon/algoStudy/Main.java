3d54f9a4eb0daba0dd2b2037966abbc5353cf9b4
lmjing <lmjing1234@gmail.com>
2018-07-05 15:43:10 +0900

    11053 코드 짜는 중 하다가 새로운 조건 발견

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,30 +5,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] input = new int[n]; // 0 : value, 1 : count
-//        int[] sorted = new int[n];
-        LinkedList<Integer> sorted = new LinkedList<>();
-
+        int[] input = new int[n];
         long[] count = new long[1000];
 
         for (int i = 0; i < n; i++) {
             input[i] = sc.nextInt();
-//            sorted[i] = input[i];
-            sorted.add(input[i]);
-            count[input[i]] = 1;
         }
-//        Arrays.sort(sorted);
-        Collections.sort(sorted);
+        count[input[0]] = 1;
+        long maxVaule = input[0];
+        long maxLength = 1;
 
         for (int i = 1; i < n; i++) {
             if (input[i-1] < input[i]) {
-                count[i] += Math.max(count[input[i-1]], count[sorted.indexOf(input[i]) - 1]);
-            }else {
-                Iterator<Integer> t = sorted.iterator();
-                
+                count[i] = Math.max(count[i], count[input[i-1]] + 1);
+                System.out.println("a : " + count[i]);
+            } else {
+                if (count[i] == 0) {
+                    int idx = i-1;
+                    while (idx >= 0 && input[idx] >= input[i]) idx--;
+                    count[i] = idx == -1 ? 1 : count[idx] + 1;
+                    System.out.println("b : " + count[i]);
+                }
             }
+            max = max < count[i] ? count[i] : max;
+            System.out.println("m : " + max);
         }
+        System.out.println(max < count[n-1] ? count[n-1] : max);
     }
 }
