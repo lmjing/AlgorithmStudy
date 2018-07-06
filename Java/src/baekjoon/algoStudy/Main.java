9a352e17f954c824b60e6893b9e4d0aed63a5309
lmjing <lmjing1234@gmail.com>
2018-07-06 16:42:12 +0900

    11055번 1차시도 bottom_up 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] input = new int[n][2]; // 0 : vaule, 1 : sum
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            int vaule = sc.nextInt();
+            input[i][0] = vaule;
+            input[i][1] = vaule;
+            for (int j = 0; j < i; j++) {
+                if (vaule > input[j][0])
+                    input[i][1] = Math.max(input[i][1], input[j][1] + vaule);
+            }
+            max = max < input[i][1] ? input[i][1] : max;
+        }
+        System.out.println(max);
     }
 }
