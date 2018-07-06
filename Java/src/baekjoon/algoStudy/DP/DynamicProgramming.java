cf9f278883f72ba6064752ba3b927168a5f41cf4
lmjing <lmjing1234@gmail.com>
2018-07-06 18:21:20 +0900

    11055번 코드 이동

--- Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
+++ Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
@@ -445,4 +445,24 @@ public class DynamicProgramming {
         }
         System.out.println(max);
     }
+
+    public static void num11055() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] input = new int[n][2]; // 0 : vaule, 1 : sum
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            int vaule = sc.nextInt();
+            input[i][0] = vaule;
+            input[i][1] = vaule;
+            for (int j = 0; j < i; j++) {
+                if (vaule > input[j][0])
+                    input[i][1] = Math.max(input[i][1], input[j][1] + vaule);
+            }
+            max = max < input[i][1] ? input[i][1] : max;
+        }
+        System.out.println(max);
+    }
 }
