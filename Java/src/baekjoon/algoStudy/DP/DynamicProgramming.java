0fe3e6d7367eb550eca3437ad91cbc04db2cb13a
lmjing <lmjing1234@gmail.com>
2018-07-07 12:59:44 +0900

    11722번 코드 이동

--- Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
+++ Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
@@ -465,4 +465,24 @@ public class DynamicProgramming {
         }
         System.out.println(max);
     }
+
+    public static void num11722() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] dp = new int[n][2];
+        int maxLength = 1;
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = sc.nextInt();
+            dp[i][1] = 1;
+            for (int j = 0; j < i; j++) {
+                if (dp[j][0] > dp[i][0])
+                    dp[i][1] = Math.max(dp[i][1], dp[j][1] + 1);
+            }
+            maxLength = Math.max(maxLength, dp[i][1]);
+        }
+
+        System.out.println(maxLength);
+    }
 }
