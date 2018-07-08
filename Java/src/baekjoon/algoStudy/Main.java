c1f320d8c8b39758faa3b0a50d43db328feff167
lmjing <lmjing1234@gmail.com>
2018-07-08 13:09:18 +0900

    1912번 2차 시도 bottom_up 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        long max = -1000000001;
+        long[] dp = new long[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int input = sc.nextInt();
+            dp[i] = Math.max(input, dp[i - 1] + input);
+            max = max < dp[i] ? dp[i] : max;
+        }
+        System.out.println(max);
     }
 }
