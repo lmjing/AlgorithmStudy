a3787c8af65834ab36dda2d4f2a9211ba65b032c
lmjing <lmjing1234@gmail.com>
2018-07-08 13:20:36 +0900

    1912번 2차 시도만에 top_down 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,17 +3,26 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
+    static int[] input;
+    static long max;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        long max = -1000000001;
-        long[] dp = new long[n + 1];
-        for (int i = 1; i <= n; i++) {
-            int input = sc.nextInt();
-            dp[i] = Math.max(input, dp[i - 1] + input);
-            max = max < dp[i] ? dp[i] : max;
-        }
+        input = new int[n + 1];
+        for (int i = 1; i <= n; i++) input[i] = sc.nextInt();
+        getMaxSum(n);
+
         System.out.println(max);
+    }
+
+    static long getMaxSum (int n) {
+        if (n == 1) {
+            max = input[n];
+            return input[n];
+        }
+        long sum = Math.max(input[n], getMaxSum(n - 1) + input[n]);
+        max = Math.max(max, sum);
+        return sum;
     }
 }
