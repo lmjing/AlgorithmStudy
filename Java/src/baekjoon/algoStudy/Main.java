ec1621a1cf712726c85a45bda7e18a84ffe41592
lmjing <lmjing1234@gmail.com>
2018-07-10 16:33:54 +0900

    11047번 하다가 아닌 것 같아서 stop

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,36 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-    }g
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int maxAble = 0; // k보다 큰 동전은 필요없으므로 시간 줄이기 위해서
+
+        int[] coin = new int[n];
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            coin[i] = sc.nextInt();
+            if (coin[i] <= k) maxAble = i;
+        }
+
+        int[][] dp = new int[maxAble + 2][2]; // 0 : count, 1 : restMoney
+        dp[maxAble + 1][1] = k;
+        for (int i = maxAble; i >= 0; i--) {
+            // 뒤에서 부터 전체 탐색
+            for (int j = maxAble + 1; j > i; j--) {
+                if (dp[j][1] >= coin[i]) {
+                    int count = k / coin[i];
+                    dp[j][] += count;
+                    k -= count * coin[i];
+                }
+            }
+
+
+            if (k >= coin[i]) {
+                int count = k / coin[i];
+                answer += count;
+                k -= count * coin[i];
+            }
+        }
+    }
 }
