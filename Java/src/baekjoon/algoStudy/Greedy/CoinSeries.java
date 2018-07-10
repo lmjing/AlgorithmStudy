7fc8e3ac3c74a68cc5b85ca9ce5f965205528fc2
lmjing <lmjing1234@gmail.com>
2018-07-10 17:10:41 +0900

    11046 코드 이동

--- Java/src/baekjoon/algoStudy/Greedy/CoinSeries.java
+++ Java/src/baekjoon/algoStudy/Greedy/CoinSeries.java
@@ -3,4 +3,23 @@ package baekjoon.algoStudy.Greedy;
 import java.util.Scanner;
 
 public class CoinSeries {
+    public static void num11047() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int maxAble = 0; // k보다 큰 동전은 필요없으므로 시간 줄이기 위해서
+
+        int[] coin = new int[n];
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            coin[i] = sc.nextInt();
+            if (coin[i] <= k) maxAble = i;
+        }
+
+        for (int i = maxAble; i >= 0; i--) {
+            answer += k / coin[i];
+            k %= coin[i];
+        }
+        System.out.println(answer);
+    }
 }
--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,22 +4,5 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int maxAble = 0; // k보다 큰 동전은 필요없으므로 시간 줄이기 위해서
-
-        int[] coin = new int[n];
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            coin[i] = sc.nextInt();
-            if (coin[i] <= k) maxAble = i;
-        }
-
-        for (int i = maxAble; i >= 0; i--) {
-            answer += k / coin[i];
-            k %= coin[i];
-        }
-        System.out.println(answer);
     }
 }
