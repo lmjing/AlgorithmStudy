ba4ff3760da68bf58a1039834ab2658a6d0234d3
lmjing <lmjing1234@gmail.com>
2018-07-12 10:18:42 +0900

    11399번 코드 이동

--- Java/src/baekjoon/algoStudy/Greedy/Greedy.java
+++ Java/src/baekjoon/algoStudy/Greedy/Greedy.java
@@ -48,4 +48,21 @@ public class Greedy {
             }
         }
     }
+
+    public static void num11399() {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+            array[i] = sc.nextInt();
+
+        Arrays.sort(array);
+        int sum = array[0];
+        for (int i = 1; i < n; i++) {
+            array[i] += array[i - 1];
+            sum += array[i];
+        }
+        System.out.println(sum);
+    }
 }
--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,19 +4,5 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++)
-            array[i] = sc.nextInt();
-
-        Arrays.sort(array);
-        int sum = array[0];
-        for (int i = 1; i < n; i++) {
-            array[i] += array[i - 1];
-            sum += array[i];
-        }
-        System.out.println(sum);
     }
 }
