8872ab7e2b5b24891907cc1a168d802312bd9e7f
lmjing <lmjing1234@gmail.com>
2018-07-13 15:01:54 +0900

    1541번 1차시도 정답

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        Stack<Integer> nums = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        int num = 0;
+        for (char c : input.toCharArray()) {
+            if (c == '-' || c == '+') {
+                operators.push(c);
+                nums.push(num);
+                num = 0;
+            } else num = num * 10 + (c - '0');
+        }
+        nums.push(num);
+
+        int answer = 0;
+        while (!operators.isEmpty()) {
+            char oper = operators.pop();
+            int su = nums.pop();
+            if (oper == '-') answer -= su;
+            else nums.push(su + nums.pop());
+        }
+        System.out.println(answer + nums.pop());
     }
 }
