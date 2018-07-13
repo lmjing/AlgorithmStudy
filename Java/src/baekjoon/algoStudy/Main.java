ee5488d7da07901d0eb083f5d7570a620a8821c0
lmjing <lmjing1234@gmail.com>
2018-07-14 01:00:32 +0900

    2875번 1차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        int team = n / 2 < m ? n / 2 : m;
+        int rest = (n + m) - team * 3;
+        k -= rest;
+        System.out.println(team - ((k + 2) / 3));
     }
 }
