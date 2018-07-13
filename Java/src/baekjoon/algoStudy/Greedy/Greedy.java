6b761b0a87d0fec43cb0396d30e942ac2bbf3eba
lmjing <lmjing1234@gmail.com>
2018-07-13 15:23:23 +0900

    1541번 코드 이동

--- Java/src/baekjoon/algoStudy/Greedy/Greedy.java
+++ Java/src/baekjoon/algoStudy/Greedy/Greedy.java
@@ -65,4 +65,57 @@ public class Greedy {
         }
         System.out.println(sum);
     }
+
+    public static class Num1541 {
+        public static void longVersion() {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+
+            Stack<Integer> nums = new Stack<>();
+            Stack<Character> operators = new Stack<>();
+            int num = 0;
+            for (char c : input.toCharArray()) {
+                if (c == '-' || c == '+') {
+                    operators.push(c);
+                    nums.push(num);
+                    num = 0;
+                } else num = num * 10 + (c - '0');
+            }
+            nums.push(num);
+
+            int answer = 0;
+            while (!operators.isEmpty()) {
+                char oper = operators.pop();
+                int su = nums.pop();
+                if (oper == '-') answer -= su;
+                else nums.push(su + nums.pop());
+            }
+            System.out.println(answer + nums.pop());
+        }
+
+        public static void shortVersion() {
+            Scanner sc = new Scanner(System.in);
+            String input = sc.nextLine();
+
+            int num = 0;
+            int sum = 0;
+            int answer = 0;
+            boolean flag = true;
+            for (char c : input.toCharArray()) {
+                int su = c - '0';
+                if (su < 0) {
+                    sum += num;
+                    num = 0;
+                    if (c == '-') {
+                        if (flag) answer += sum;
+                        else answer -= sum;
+                        sum = 0;
+                        flag = false;
+                    }
+                } else num = num * 10 + (c - '0');
+            }
+            sum += num;
+            System.out.println(answer + (flag ? sum : -1 * sum));
+        }
+    }
 }
--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,27 +4,5 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-
-        int num = 0;
-        int sum = 0;
-        int answer = 0;
-        boolean flag = true;
-        for (char c : input.toCharArray()) {
-            int su = c - '0';
-            if (su < 0) {
-                sum += num;
-                num = 0;
-                if (c == '-') {
-                    if (flag) answer += sum;
-                    else answer -= sum;
-                    sum = 0;
-                    flag = false;
-                }
-            } else num = num * 10 + (c - '0');
-        }
-        sum += num;
-        System.out.println(answer + (flag ? sum : -1 * sum));
     }
 }
