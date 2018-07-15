e12d72661ab4cbd258cd2ee65d8483c2da0a02be
lmjing <lmjing1234@gmail.com>
2018-07-15 16:01:34 +0900

    1744번 코드 이동

--- Java/src/baekjoon/algoStudy/Greedy/Greedy.java
+++ Java/src/baekjoon/algoStudy/Greedy/Greedy.java
@@ -167,4 +167,33 @@ public class Greedy {
             System.out.println(team);
         }
     }
+
+    public static void num1744(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] input = new int[n];
+
+        int minusCnt = 0;
+        for (int i = 0; i < n; i++) {
+            input[i] = sc.nextInt();
+            if (input[i] <= 0) minusCnt++;
+        }
+
+        Arrays.sort(input);
+        int i = -1;
+        int answer = 0;
+
+        while (++i < minusCnt) {
+            if (i + 1 < minusCnt) answer += input[i] * input[++i];
+            else answer += input[i];
+        }
+
+        i = n;
+        while (--i >= minusCnt) {
+            if (i - 1 >= minusCnt && input[i - 1] > 1)
+                answer += input[i] * input[--i];
+            else answer += input[i];
+        }
+        System.out.println(answer);
+    }
 }
--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,31 +4,5 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] input = new int[n];
-
-        int minusCnt = 0;
-        for (int i = 0; i < n; i++) {
-            input[i] = sc.nextInt();
-            if (input[i] <= 0) minusCnt++;
-        }
-
-        Arrays.sort(input);
-        int i = -1;
-        int answer = 0;
-
-        while (++i < minusCnt) {
-            if (i + 1 < minusCnt) answer += input[i] * input[++i];
-            else answer += input[i];
-        }
-
-        i = n;
-        while (--i >= minusCnt) {
-            if (i - 1 >= minusCnt && input[i - 1] > 1)
-                answer += input[i] * input[--i];
-            else answer += input[i];
-        }
-        System.out.println(answer);
     }
 }
