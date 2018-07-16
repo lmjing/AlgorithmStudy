d9ec1ea5adb183053e8dbe0a008e5379754a95db
lmjing <lmjing1234@gmail.com>
2018-07-16 12:36:12 +0900

    5525번 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,23 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        int m = Integer.parseInt(sc.nextLine());
+        char[] s = sc.nextLine().toCharArray();
+
+        int result = 0;
+        int patternCnt = 0;
+        for (int i = 1; i < m - 1; i++) {
+            if (s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+                patternCnt++;
+                if (patternCnt == n) {
+                    patternCnt--;
+                    result++;
+                }
+                i++;
+            } else patternCnt = 0;
+        }
+        System.out.println(result);
     }
 }
