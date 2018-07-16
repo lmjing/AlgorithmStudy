c8ae70a161e75727122ab26854e2e64ed55689d8
lmjing <lmjing1234@gmail.com>
2018-07-16 10:50:48 +0900

    2902번 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] inputs = sc.nextLine().split("-");
+        StringBuilder result = new StringBuilder("");
+        for (String str : inputs)
+            result.append(str.charAt(0));
+
+        System.out.println(result);
     }
 }
