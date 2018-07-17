46fd4460e0e0135aa18942909b0e6b1ca98673e1
lmjing <lmjing1234@gmail.com>
2018-07-17 13:36:06 +0900

    1786번 KMP 적용 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,40 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] T = sc.nextLine().toCharArray();
+        char[] P = sc.nextLine().toCharArray();
+
+        int[] p = new int[P.length];
+        for (int i = 1; i < P.length; i++) {
+            int compareIdx = p[i - 1];
+            while (compareIdx > 0 && P[compareIdx] != P[i])
+                compareIdx = p[compareIdx - 1];
+
+            if (P[compareIdx] == P[i])
+                p[i] = compareIdx + 1;
+        }
+
+        int i = -1;
+        int j = 0;
+        int count = 0;
+        StringBuilder answer = new StringBuilder("");
+        while (++i < T.length) {
+            while (j > 0 && T[i] != P[j]) {
+                j = p[j - 1];
+            }
+
+            if (T[i] == P[j]) {
+                j++;
+                if (j == P.length) {
+                    count++;
+                    answer.append((i - P.length + 2) + " ");
+                    j = p[j - 1];
+                }
+            }
+        }
+
+        System.out.println(count);
+        System.out.println(answer);
     }
 }
