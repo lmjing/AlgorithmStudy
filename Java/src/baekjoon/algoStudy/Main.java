95f7ce9b9387baae9f5bdcd41d0fe4c0dc90f4ba
lmjing <lmjing1234@gmail.com>
2018-07-18 16:30:29 +0900

    1305번 kmp 답 보고 적용  4차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -10,17 +10,14 @@ public class Main {
 
         int i = 0;
         int[] pi = new int[L];
-        int max = 0;
         while (++i < L) {
             int compareIdx = pi[i - 1];
             while (compareIdx > 0 && screen[i] != screen[compareIdx])
                 compareIdx = pi[compareIdx - 1];
 
-            if (screen[i] == screen[compareIdx]) {
+            if (screen[i] == screen[compareIdx])
                 pi[i] = compareIdx + 1;
-                max = max < pi[i] ? pi[i] : max;
-            }
         }
-        System.out.println(L - max);
+        System.out.println(L - pi[L - 1]);
     }
 }
