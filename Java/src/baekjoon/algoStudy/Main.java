386166a2fb8ee76a414c73715c29590abd13ff92
lmjing <lmjing1234@gmail.com>
2018-07-19 15:46:16 +0900

    1701번 답지 참고 짧은 버전 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -6,34 +6,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char[] p = sc.nextLine().toCharArray();
-        int[] pi = new int[p.length];
-        int[] alphabet = new int[26];
+        int result = 0;
 
-        int i = 0;
-        int maxLength = 0;
-        int startIdx = 0;
-        int compareIdx = 0;
+        for (int s = 0; s < p.length; s++) {
+            int i = 0;
+            int maxLength = 0;
+            int size = p.length - s;
+            int[] pi = new int[size];
+            int compareIdx = 0;
+            while (++i < size) {
+                while (compareIdx > 0 && p[compareIdx + s] != p[i + s])
+                    compareIdx = pi[compareIdx - 1];
 
-        for (int k = 0; k < 26; k++)
-            alphabet[k] = -1;
-//        System.out.print(pi[i]);
-        while (++i < pi.length) {
-            while (compareIdx > 0 && p[compareIdx] != p[i])
-                compareIdx = pi[compareIdx - 1];
+                if (p[compareIdx + s] == p[i + s]) {
+                    pi[i] = ++compareIdx;
+                    if (maxLength < pi[i]) maxLength = pi[i];
+                }
+            }
 
-            if (p[compareIdx] == p[i]) {
-                pi[i] = ++compareIdx - startIdx;
-                if (maxLength < pi[i]) maxLength = pi[i];
-            } else if (alphabet[p[i] - 'a'] > -1){
-                startIdx = alphabet[p[i] - 'a'];
-                compareIdx = startIdx + 1;
-                pi[i] = 1;
-                if (maxLength < 1) maxLength = 1;
-            } else alphabet[p[i] - 'a'] = i;
-//                charIdxs.put(p[i], i);
-//            System.out.print(pi[i]);
+            if (result < maxLength) result = maxLength;
         }
-//        System.out.println("");
-        System.out.println(maxLength);
+        System.out.println(result);
     }
 }
