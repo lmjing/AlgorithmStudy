0ea46295a99671db557d6abbe82320adf2864a37
lmjing <lmjing1234@gmail.com>
2018-07-19 14:03:28 +0900

    1305번 코드 이동

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,20 +4,5 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int L = Integer.parseInt(sc.nextLine());
-        char[] screen = sc.nextLine().toCharArray();
-
-        int i = 0;
-        int[] pi = new int[L];
-        while (++i < L) {
-            int compareIdx = pi[i - 1];
-            while (compareIdx > 0 && screen[i] != screen[compareIdx])
-                compareIdx = pi[compareIdx - 1];
-
-            if (screen[i] == screen[compareIdx])
-                pi[i] = compareIdx + 1;
-        }
-        System.out.println(L - pi[L - 1]);
     }
 }
--- Java/src/baekjoon/algoStudy/UseString/FindPattern.java
+++ Java/src/baekjoon/algoStudy/UseString/FindPattern.java
@@ -142,4 +142,22 @@ public class FindPattern {
         }
     }
 
+    public static void num1305() {
+        Scanner sc = new Scanner(System.in);
+        int L = Integer.parseInt(sc.nextLine());
+        char[] screen = sc.nextLine().toCharArray();
+
+        int i = 0;
+        int[] pi = new int[L];
+        while (++i < L) {
+            int compareIdx = pi[i - 1];
+            while (compareIdx > 0 && screen[i] != screen[compareIdx])
+                compareIdx = pi[compareIdx - 1];
+
+            if (screen[i] == screen[compareIdx])
+                pi[i] = compareIdx + 1;
+        }
+        System.out.println(L - pi[L - 1]);
+    }
+
 }
