b9e7214eb7aa7d69ebcee3e8554053c20eec111c
lmjing <lmjing1234@gmail.com>
2018-07-20 13:53:46 +0900

    14425번 시간 초과

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        StringBuffer S = new StringBuffer();
+        for (int i = 0; i < n; i++)
+            S.append(sc.nextLine() + " ");
+
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            String str = sc.nextLine();
+            if (S.toString().contains(str)) result++;
+        }
+        System.out.println(result);
     }
 }
