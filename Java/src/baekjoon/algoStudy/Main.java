875404b8418ede87ca9a78e8bd51413fbf0bd882
lmjing <lmjing1234@gmail.com>
2018-07-23 13:16:13 +0900

    num2579번 top-down 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,15 +3,25 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
+    static int[][] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] dp = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            int step = sc.nextInt();
-            dp[i][0] = (i > 1 ? Math.max(dp[i - 2][1], dp[i - 2][0]) : 0) + step;
-            dp[i][1] = (i > 0 ? dp[i - 1][0] : 0) + step;
+        dp = new int[n + 1][2];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i][0] = dp[i][1] = sc.nextInt();
         }
-        System.out.println(Math.max(dp[n - 1][0], dp[n - 1][1]));
+
+        goTo(n);
+        System.out.println(Math.max(dp[n][0], dp[n][1]));
+    }
+
+    static void goTo (int i) {
+        if (i <= 1) return;
+        goTo(i - 1);
+        if (i > 2)
+            dp[i][0] += Math.max(dp[i - 2][0], dp[i - 2][1]);
+        dp[i][1] += dp[i - 1][0];
     }
 }
