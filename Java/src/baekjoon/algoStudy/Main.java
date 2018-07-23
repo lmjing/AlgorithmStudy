a27ce99767fe1ae1052d417ee6eda74cbfd394cf
lmjing <lmjing1234@gmail.com>
2018-07-23 12:22:43 +0900

    num2579번 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            int step = sc.nextInt();
+            dp[i][0] = (i > 1 ? Math.max(dp[i - 2][1], dp[i - 2][0]) : 0) + step;
+            dp[i][1] = (i > 0 ? dp[i - 1][0] : 0) + step;
+        }
+        System.out.println(Math.max(dp[n - 1][0], dp[n - 1][1]));
     }
 }
