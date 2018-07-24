6c7c15c1a045145be0511fba75663d39b0ec351b
lmjing <lmjing1234@gmail.com>
2018-07-24 13:02:59 +0900

    2133번 top_down 메모리 줄임

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,21 +3,23 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
+    static int[] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        dp = new int[n / 2 + 1];
+        System.out.println(n % 2 == 0 ? goTo(n) : 0);
+    }
 
-        if (n % 2 == 0) {
-            int[] dp = new int[n / 2 + 1];
-            dp[1] = 3;
-            dp[0] = 1;
-            for (int i = 4; i <= n; i += 2) {
-                for (int j = 2; j <= i; j += 2) {
-                    int standard = j == 2 ? 3 : 2;
-                    dp[i / 2] += standard * dp[(i - j) / 2];
-                }
+    static int goTo (int n) {
+        if (n == 0) return 1;
+        if (n == 2) return 3;
+        else if (dp[n / 2] == 0) {
+            for (int i = 2; i <= n; i += 2) {
+                int standard = i == 2 ? 3 : 2;
+                dp[n / 2] += standard * goTo(n - i);
             }
-            System.out.println(dp[n / 2]);
-        } else System.out.println(0);
+        }
+        return dp[n / 2];
     }
 }
