734f58ce1b0b000297dcf9dc66b1b234e300daae
lmjing <lmjing1234@gmail.com>
2018-07-24 12:59:12 +0900

    2133번 bottom_up 메모리 줄임

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,25 +3,21 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
-    static int[] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        dp = new int[n + 1];
 
-        int answer = 0;
         if (n % 2 == 0) {
-            dp[2] = 3;
+            int[] dp = new int[n / 2 + 1];
+            dp[1] = 3;
             dp[0] = 1;
             for (int i = 4; i <= n; i += 2) {
                 for (int j = 2; j <= i; j += 2) {
                     int standard = j == 2 ? 3 : 2;
-                    dp[i] += standard * dp[i - j];
+                    dp[i / 2] += standard * dp[(i - j) / 2];
                 }
             }
-            answer = dp[n];
-        }
-
-        System.out.println(answer);
+            System.out.println(dp[n / 2]);
+        } else System.out.println(0);
     }
 }
