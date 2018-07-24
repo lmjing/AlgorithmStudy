5e98147b85cb20c1966244688d932de242b08a47
lmjing <lmjing1234@gmail.com>
2018-07-24 12:42:05 +0900

    2133번 top_down 1차시도 정답

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,6 +3,23 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
+    static int[] dp;
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dp = new int[n + 1];
+        System.out.println(n % 2 == 0 ? goTo(n) : 0);
+    }
+
+    static int goTo (int n) {
+        if (n == 0) return 1;
+        if (n == 2) dp[2] = 3;
+        else if (dp[n] == 0) {
+            for (int i = 2; i <= n; i += 2) {
+                int standard = i == 2 ? 3 : 2;
+                dp[n] += standard * goTo(n - i);
+            }
+        }
+        return dp[n];
     }
 }
