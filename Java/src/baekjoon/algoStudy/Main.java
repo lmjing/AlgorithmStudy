777317b8c72e2427151a5fbfea15992f41b7406c
lmjing <lmjing1234@gmail.com>
2018-07-26 00:10:34 +0900

    9461 3차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -9,17 +9,20 @@ public class Main {
         int T = sc.nextInt();
         dp = new int[101];
         dp[1] = dp[2] = 1;
+        for (int i = 3; i <= 100; i++) {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
         for (int i = 0; i < T; i++) {
-            System.out.println(goTo(sc.nextInt()));
+            System.out.println(dp[sc.nextInt()]);
         }
     }
 
-    static int goTo (int n) {
-        if (n == 0) dp[n] = 0;
-        else if (n == 1 || n == 2) dp[n] = 1;
-        else if (dp[n] == 0) {
-            dp[n] = goTo(n - 2) + goTo(n - 3);
-        }
-        return dp[n];
-    }
+//    static int goTo (int n) {
+//        if (n == 0) dp[n] = 0;
+//        else if (n == 1 || n == 2) dp[n] = 1;
+//        else if (dp[n] == 0) {
+//            dp[n] = goTo(n - 2) + goTo(n - 3);
+//        }
+//        return dp[n];
+//    }
 }
