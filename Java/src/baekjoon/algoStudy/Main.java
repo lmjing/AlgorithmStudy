dd11ad31dc62641180a829e7a2ec3d0999894138
lmjing <lmjing1234@gmail.com>
2018-07-26 22:21:22 +0900

    9461 엄마 방법 top_down 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -8,21 +8,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         dp = new long[101];
-        dp[1] = dp[2] = 1;
-        for (int i = 3; i <= 100; i++) {
-            dp[i] = dp[i - 2] + dp[i - 3];
-        }
         for (int i = 0; i < T; i++) {
-            System.out.println(dp[sc.nextInt()]);
+            System.out.println(goTo(sc.nextInt()));
         }
     }
 
-//    static int goTo (int n) {
-//        if (n == 0) dp[n] = 0;
-//        else if (n == 1 || n == 2) dp[n] = 1;
-//        else if (dp[n] == 0) {
-//            dp[n] = goTo(n - 2) + goTo(n - 3);
-//        }
-//        return dp[n];
-//    }
+    static long goTo (int n) {
+        if (n == 1 || n == 2) dp[n] = 1;
+        else if (dp[n] == 0 && n > 0) {
+            dp[n] = goTo(n - 2) + goTo(n - 3);
+        }
+        return dp[n];
+    }
 }
