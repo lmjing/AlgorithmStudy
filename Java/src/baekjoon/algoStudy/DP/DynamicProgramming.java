9d6e5fb866054057b80bd6935de72ecd6670541a
lmjing <lmjing1234@gmail.com>
2018-07-26 22:30:41 +0900

    9461 코드 이동

--- Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
+++ Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
@@ -654,4 +654,62 @@ public class DynamicProgramming {
             return dp[n];
         }
     }
+
+    static class Num9461 {
+        static long[] dp = new long[101];
+        public static void momWay_bottomUp() {
+            Scanner sc = new Scanner(System.in);
+            int T = sc.nextInt();
+
+            dp[1] = dp[2] = 1;
+            for (int i = 3; i <= 100; i++)
+                dp[i] = dp[i - 2] + dp[i - 3];
+
+            for (int i = 0; i < T; i++)
+                System.out.println(dp[sc.nextInt()]);
+        }
+
+        public static void momWay_topDown() {
+            Scanner sc = new Scanner(System.in);
+            int T = sc.nextInt();
+            for (int i = 0; i < T; i++)
+                System.out.println(goTo(sc.nextInt()));
+        }
+
+        static long goTo (int n) {
+            if (n == 1 || n == 2) dp[n] = 1;
+            else if (dp[n] == 0 && n > 0)
+                dp[n] = goTo(n - 2) + goTo(n - 3);
+            return dp[n];
+        }
+
+        public static void myWay_bottomUp() {
+            Scanner sc = new Scanner(System.in);
+            int T = sc.nextInt();
+            dp[1] = dp[2] = dp[3] = 1;
+            dp[4] = dp[5] = 2;
+            for (int i = 6; i <= 100; i++)
+                dp[i] = dp[i - 1] + dp[i - 5];
+
+            for (int i = 0; i < T; i++)
+                System.out.println(dp[sc.nextInt()]);
+
+        }
+
+        public static void myWay_topDown() {
+            Scanner sc = new Scanner(System.in);
+            int T = sc.nextInt();
+            for (int i = 0; i < T; i++)
+                System.out.println(goTo2(sc.nextInt()));
+
+        }
+
+        static long goTo2 (int n) {
+            if (n == 1 || n == 2 || n == 3) dp[n] = 1;
+            else if (n == 4 || n == 5) dp[n] = 2;
+            else if (dp[n] == 0 && n > 0)
+                dp[n] = goTo(n - 1) + goTo(n - 5);
+            return dp[n];
+        }
+    }
 }
--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,21 +3,6 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
-    static long[] dp = new long[101];
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int i = 0; i < T; i++)
-            System.out.println(goTo(sc.nextInt()));
-
-    }
-
-    static long goTo (int n) {
-        if (n == 1 || n == 2 || n == 3) dp[n] = 1;
-        else if (n == 4 || n == 5) dp[n] = 2;
-        else if (dp[n] == 0 && n > 0) {
-            dp[n] = goTo(n - 1) + goTo(n - 5);
-        }
-        return dp[n];
     }
 }
