86bcf6b5217a3a8840e0d8a6abf7ecd2aec22374
lmjing <lmjing1234@gmail.com>
2018-07-26 22:25:02 +0900

    9461 내 방법 bottom_up 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -8,16 +8,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
         dp = new long[101];
-        for (int i = 0; i < T; i++) {
-            System.out.println(goTo(sc.nextInt()));
-        }
+        dp[1] = dp[2] = dp[3] = 1;
+        dp[4] = dp[5] = 2;
+        for (int i = 6; i <= 100; i++)
+            dp[i] = dp[i - 1] + dp[i - 5];
+
+        for (int i = 0; i < T; i++)
+            System.out.println(dp[sc.nextInt()]);
+
     }
 
-    static long goTo (int n) {
-        if (n == 1 || n == 2) dp[n] = 1;
-        else if (dp[n] == 0 && n > 0) {
-            dp[n] = goTo(n - 2) + goTo(n - 3);
-        }
-        return dp[n];
-    }
+//    static long goTo (int n) {
+//        if (n == 1 || n == 2) dp[n] = 1;
+//        else if (dp[n] == 0 && n > 0) {
+//            dp[n] = goTo(n - 1) + goTo(n - 3);
+//        }
+//        return dp[n];
+//    }
 }
