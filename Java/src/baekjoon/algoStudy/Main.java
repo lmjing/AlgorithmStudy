a768052f7baadaa4c71bb695d6698b40fd5770c1
lmjing <lmjing1234@gmail.com>
2018-07-27 15:24:46 +0900

    2225번 bottom_up 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,23 +3,21 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
-    static long[][] dp;
+    static int[][] dp;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        dp = new long[n + 1][k + 1];
-        System.out.println(goTo(n, k));
-    }
-
-    static long goTo (int n, int k) {
-        if (n == 1)
-            dp[n][k] = k;
-        else if (k == 1)
-            dp[n][k] = 1;
-        else if (dp[n][k] == 0)
-            dp[n][k] = goTo(n - 1, k) + goTo(n, k - 1);
-       return dp[n][k] % 1000000000;
+        dp = new int[n + 1][k + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (i == 1) dp[i][j] = j;
+                else if (j == 1) dp[i][j] = 1;
+                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                dp[i][j] %= 1000000000;
+            }
+        }
+        System.out.println(dp[n][k]);
     }
 }
