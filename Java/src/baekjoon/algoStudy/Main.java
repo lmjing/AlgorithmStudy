36ae89750784fae44628df2851cd07e8660fc3f8
lmjing <lmjing1234@gmail.com>
2018-07-27 15:15:38 +0900

    2225번 시간초과

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,6 +3,23 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
+    static long[][] dp;
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        dp = new long[n + 1][k + 1];
+        System.out.println(goTo(n, k));
+    }
+
+    static long goTo (int n, int k) {
+        if (n == 1)
+            dp[n][k] = k;
+        else if (k == 1)
+            dp[n][k] = 1;
+        else
+            dp[n][k] = goTo(n - 1, k) + goTo(n, k - 1);
+       return dp[n][k] % 1000000000;
     }
 }
