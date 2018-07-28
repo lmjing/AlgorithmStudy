b935c44d285956b6b891a01af54479a7595ed0bc
lmjing <lmjing1234@gmail.com>
2018-07-28 13:26:15 +0900

    2011 bottom_up 6차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,18 +5,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] input = sc.nextLine().toCharArray();
+        String input = sc.nextLine();
+//        char[] input = sc.nextLine().toCharArray();
         int mod = 1000000;
-        int[] dp = new int[input.length + 1];
+        int[] dp = new int[input.length() + 1];
         dp[0] = dp[1] = 1;
-        for (int i = 2; i <= input.length; i++) {
+        for (int i = 2; i <= input.length(); i++) {
             int now = i - 1;
-            if (input[now] > '0')
+            if (input.charAt(now) > '0')
                 dp[i] = dp[i - 1];
-            int su = (input[now - 1] - '0') * 10 + (input[now] - '0');
+            int su = (input.charAt(now - 1) - '0') * 10 + (input.charAt(now) - '0');
             if (su >= 10 && su <= 26)
                 dp[i] = (dp[i] + dp[i - 2]) % mod;
         }
-        System.out.println(dp[input.length]);
+        System.out.println(input != "0" ? dp[input.length()] : 0);
     }
 }
