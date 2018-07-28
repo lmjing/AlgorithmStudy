e43de75acef47e7ecb1a09312b2fbf4b31c3a0c2
lmjing <lmjing1234@gmail.com>
2018-07-28 13:34:46 +0900

    2011 코드 이동

--- Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
+++ Java/src/baekjoon/algoStudy/DP/DynamicProgramming.java
@@ -751,4 +751,21 @@ public class DynamicProgramming {
             System.out.println(dp[n][k]);
         }
     }
+
+    public static void num2011() {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int mod = 1000000;
+        int[] dp = new int[input.length() + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= input.length(); i++) {
+            int now = i - 1;
+            if (input.charAt(now) > '0')
+                dp[i] = dp[i - 1];
+            int su = (input.charAt(now - 1) - '0') * 10 + (input.charAt(now) - '0');
+            if (su >= 10 && su <= 26)
+                dp[i] = (dp[i] + dp[i - 2]) % mod;
+        }
+        System.out.println(input.equals("0") ? 0 : dp[input.length()]);
+    }
 }
--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,20 +4,5 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-//        char[] input = sc.nextLine().toCharArray();
-        int mod = 1000000;
-        int[] dp = new int[input.length() + 1];
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i <= input.length(); i++) {
-            int now = i - 1;
-            if (input.charAt(now) > '0')
-                dp[i] = dp[i - 1];
-            int su = (input.charAt(now - 1) - '0') * 10 + (input.charAt(now) - '0');
-            if (su >= 10 && su <= 26)
-                dp[i] = (dp[i] + dp[i - 2]) % mod;
-        }
-        System.out.println(input.equals("0") ? 0 : dp[input.length()]);
     }
 }
