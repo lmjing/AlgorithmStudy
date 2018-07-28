f7954dc83a828283e206124b0318617e645eb045
lmjing <lmjing1234@gmail.com>
2018-07-28 13:18:04 +0900

    2011 bottom_up 4차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,17 +5,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-//        char[] input = sc.nextLine().toCharArray();
+        char[] input = sc.nextLine().toCharArray();
         int mod = 1000000;
-        int[] dp = new int[input.length() + 1];
+        int[] dp = new int[input.length + 1];
         dp[0] = dp[1] = 1;
-        for (int i = 2; i <= input.length(); i++) {
-//            if (input[i - 2] <= '2' && input[i - 1] <= '6')
-            if (input.charAt(i - 2) <= '2' && input.charAt(i - 1) <= '6')
-                dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
-            else dp[i] = dp[i - 1];
+        for (int i = 2; i <= input.length; i++) {
+            int now = i - 1;
+            if (input[now] > '0')
+                dp[i] = dp[i - 1];
+            int su = (input[now - 1] - '0') * 10 + (input[now] - '0');
+            if (su > 0 && su <= 26)
+                dp[i] = (dp[i] + dp[i - 2]) % mod;
         }
-        System.out.println(dp[input.length()]);
+        System.out.println(dp[input.length]);
     }
 }
