9897d7f208b31dd280e0b845659398e8b4426673
lmjing <lmjing1234@gmail.com>
2018-07-28 12:40:59 +0900

    2011 bottom_up 1차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] input = sc.nextLine().toCharArray();
+        int mod = 1000000;
+        int[][] dp = new int[input.length][2];
+        dp[0] = new int[]{1, 0};
+        for (int i = 1; i < input.length; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+            if (input[i - 1] <= '2' && input[i] <= '6')
+                dp[i][1] = dp[i - 1][0];
+        }
+        System.out.println((dp[input.length - 1][0] + dp[input.length - 1][1]) % mod);
     }
 }
