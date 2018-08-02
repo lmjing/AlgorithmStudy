f7950fd852677901da6e29be6f33a5eb884c63ea
lmjing <lmjing1234@gmail.com>
2018-08-03 00:19:31 +0900

    1783 코드 이동

--- Java/src/baekjoon/algoStudy/Greedy/Greedy.java
+++ Java/src/baekjoon/algoStudy/Greedy/Greedy.java
@@ -196,4 +196,18 @@ public class Greedy {
         }
         System.out.println(answer);
     }
+
+    public static void num1783() {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+        long m = sc.nextInt();
+
+        if (n == 1) System.out.println(1);
+        else if (n == 2)
+            System.out.println(Math.min(4, (m + 1) / 2));
+        else if (m < 7)
+            System.out.println(Math.min(4, m));
+        else
+            System.out.println(m - 2);
+    }
 }
