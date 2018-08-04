0ae311d140ff4ea30d9f7c2e4f97c6e357a57cc8
lmjing <lmjing1234@gmail.com>
2018-08-04 13:04:29 +0900

    .

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,67 +5,57 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int[] answer = solution.solution(6, 4, new int[][]{{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}});
-//        System.out.println(answer[0] + " " + answer[1]);
+        int[] answer = solution.solution(6, 4, new int[][]{{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}});
+        System.out.println(answer[0] + " " + answer[1]);
 
         int[] answer2 = solution.solution(6, 4, new int[][]{{1, 1, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}});
         System.out.println(answer2[0] + " " + answer2[1]);
     }
 
     static class Solution {
-        class Position {
-            int x, y;
-            public Position(int x, int y) {
-                this.x = x;
-                this.y = y;
+        int m, n;
+        int[][] picture;
+        boolean[][] visit;
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        public int checkArea(int x, int y) {
+            int result = 1;
+            visit[x][y] = true;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                System.out.println(nx + " " + ny);
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && picture[x][y] == picture[nx][nx])
+                    result += checkArea(nx, ny);
             }
+            return result;
         }
 
-        int[] dx = {0, 1};
-        int[] dy = {1, 0};
-        boolean [][] check;
-
         public int[] solution(int m, int n, int[][] picture) {
-            check = new boolean[m][n];
-
-            Queue<Position> queue = new LinkedList<>();
-            queue.add(new Position(0, 0));
+            this.m = m;
+            this.n = n;
+            this.picture = picture;
+            visit = new boolean[m][n];
 
             int numberOfArea = 0;
             int maxSizeOfOneArea = 0;
-            while (!queue.isEmpty()) {
-                Position p = queue.remove();
-                if (check[p.x][p.y]) continue;
 
-                int v = picture[p.x][p.y];
-                int count = 0;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (picture[i][j] == 0 || visit[i][j]) continue;
 
-                Queue<Position> temp = new LinkedList<>();
-                temp.add(p);
-
-                while (!temp.isEmpty()) {
-                    Position cur = temp.remove();
-                    if (check[cur.x][cur.y]) continue;
-
-                    check[cur.x][cur.y] = true;
-                    count++;
-                    for (int i = 0; i < 2; i++) {
-                        int nx = cur.x + dx[i];
-                        int ny = cur.y + dy[i];
-                        if (nx >= 0 && nx < m && ny >= 0 && ny < n && !check[nx][ny]) {
-                            if (picture[nx][ny] != v) queue.add(new Position(nx, ny));
-                            else temp.add(new Position(nx, ny));
-                        }
-                    }
-                }
-
-                if (v > 0) {
+                    int count = checkArea(i, j);
                     if (maxSizeOfOneArea < count) maxSizeOfOneArea = count;
                     numberOfArea++;
                 }
             }
 
-            return new int[] {numberOfArea, maxSizeOfOneArea};
+            int[] answer = new int[2];
+            answer[0] = numberOfArea;
+            answer[1] = maxSizeOfOneArea;
+            return answer;
         }
     }
 }
