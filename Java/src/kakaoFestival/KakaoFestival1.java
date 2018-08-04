bb962e8a5280c0df3432a12fca0b4a6361a50405
lmjing <lmjing1234@gmail.com>
2018-08-04 14:33:18 +0900

    2번풀었는데 출력 자르기 못하겠음

--- Java/src/kakaoFestival/KakaoFestival1.java
+++ Java/src/kakaoFestival/KakaoFestival1.java
@@ -1,9 +1,42 @@
-package baekjoon.algoStudy;
+package kakaoFestival;
 
 import java.text.DecimalFormat;
 import java.util.*;
 
-public class Main {
+public class KakaoFestival1 {
+    public static void num1(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        int[][] festival1 = {{500, 300, 200, 50, 30, 10}, {1, 2, 3, 4, 5, 6}};
+        int[][] festival2 = {{512, 256, 128, 64, 32}, {1, 2, 4, 8, 16}};
+
+        for (int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            long money = 0;
+            int aSum = 0;
+            for (int j = 0; j < 6 && a > 0; j++) {
+                aSum += festival1[1][j];
+                if (a <= aSum) {
+                    money += festival1[0][j] * 10000;
+                    break;
+                }
+            }
+
+            int bSum = 0;
+            for (int j = 0; j < 5 && b > 0; j++) {
+                bSum += festival2[1][j];
+                if (b <= bSum) {
+                    money += festival2[0][j] * 10000;
+                    break;
+                }
+            }
+            System.out.println(money);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
