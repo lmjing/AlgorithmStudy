7bf3fdea62642c24bb1caa5eb08bf4cd60ed701f
lmjing <lmjing1234@gmail.com>
2018-08-08 16:27:44 +0900

    10815 이진탐색 코드 수정

--- Java/src/baekjoon/algoStudy/DivideAndConquer.java
+++ Java/src/baekjoon/algoStudy/DivideAndConquer.java
@@ -3,14 +3,22 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class DivideAndConquer {
-    static int binary_search (int[] array, int find, int s, int e) {
+    static int binary_search (int[] array, int find) {
+        // NOTE : 찾은 idx 리턴하는 함수 (추후 활용하게 idx로 만듦)
         // NOTE : 전제 : array는 정렬된 배열이다.
-        if (s > e) return -1;
+        int l = 0;
+        int r = array.length - 1;
 
-        int half = s + (e - s) / 2;
-        if (array[half] == find) return half;
-        else if (array[half] < find) return binary_search(array, find, half + 1, e);
-        else return binary_search(array, find, s, half - 1);
+        while (l <= r) {
+            int half = (l + r) / 2;
+            if (array[half] == find)
+                return half;
+            else if (array[half] < find)
+                l = half + 1;
+            else
+                r = half - 1;
+        }
+        return -1;
     }
 
     public static void num10815 () {
@@ -24,7 +32,7 @@ public class DivideAndConquer {
 
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
-            System.out.print((binary_search(haved, sc.nextInt(), 0, m - 1) > -1 ? 1 : 0) + " ");
+            System.out.print((binary_search(haved, sc.nextInt()) > -1 ? 1 : 0) + " ");
         }
     }
 }
