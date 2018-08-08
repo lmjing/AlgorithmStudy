ab583e11b51aa187dc6d732d1605e987e00e328c
lmjing <lmjing1234@gmail.com>
2018-08-08 16:10:13 +0900

    10815 이분탐색 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+<<<<<<< Updated upstream
         int n = sc.nextInt();
 
         Node[] array = new Node[n];
@@ -41,5 +42,27 @@ public class Main {
                 return this.idx - o.idx;
             }else return this.vaule - o.vaule;
         }
+=======
+        int m = sc.nextInt();
+        int[] haved = new int[m];
+        for (int i = 0; i < m; i++)
+            haved[i] = sc.nextInt();
+
+        Arrays.sort(haved);
+
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print((binary_search(haved, sc.nextInt(), 0, m - 1) > -1 ? 1 : 0) + " ");
+        }
+    }
+
+    static int binary_search (int[] array, int find, int s, int e) {
+        if (s > e) return -1;
+
+        int half = s + (e - s) / 2;
+        if (array[half] == find) return half;
+        else if (array[half] < find) return binary_search(array, find, half + 1, e);
+        else return binary_search(array, find, s, half - 1);
+>>>>>>> Stashed changes
     }
 }
