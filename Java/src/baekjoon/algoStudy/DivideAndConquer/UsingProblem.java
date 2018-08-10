95fc39b03a0c9d5dbf3c23f69bc4a03d6202a83b
lmjing <lmjing1234@gmail.com>
2018-08-10 23:38:50 +0900

    1780 코드 이동

---
+++ Java/src/baekjoon/algoStudy/DivideAndConquer/UsingProblem.java
@@ -0,0 +1,47 @@
+package baekjoon.algoStudy.DivideAndConquer;
+
+import java.util.*;
+
+public class UsingProblem {
+    public class Num1780 {
+        int[][] map;
+        public void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            map = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    int input = sc.nextInt();
+                    if (input == -1) input = 2;
+                    map[i][j] = input;
+                }
+            }
+            int[] result = getCount(new int[]{0, 0}, n);
+            System.out.println(result[2]);
+            System.out.println(result[0]);
+            System.out.println(result[1]);
+        }
+
+        int[] getCount (int[] s, int n) {
+            int[] count = new int[3];
+            if (n == 1) {
+                count[map[s[0]][s[1]]]++;
+                return count;
+            }
+            int last = 3;
+            for (int i = s[0]; i < s[0] + n; i += n / 3) {
+
+                for (int j = s[1]; j < s[1] + n; j += n / 3) {
+                    int[] result = getCount(new int[]{i, j}, n / 3);
+                    for (int k = 0; k < 3; k++) {
+                        if (result[k] > 0) last = k;
+                        count[k] += result[k];
+                    }
+                }
+            }
+            if (count[last] == 9)
+                count[last] = 1;
+            return count;
+        }
+    }
+}
