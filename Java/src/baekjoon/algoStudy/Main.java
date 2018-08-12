4d2047eaabf9d71156816c0ff3a11b3cdb28ada4
lmjing <lmjing1234@gmail.com>
2018-08-12 16:32:08 +0900

    2580 4차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,9 +4,8 @@ import java.util.*;
 
 public class Main {
     static int[][] sudoku = new int[9][9];
-    static boolean[][][] check = new boolean[3][9][9];
+    static boolean[][][] check = new boolean[9][9][9];
     static LinkedList<Point> emptyPoints = new LinkedList<>();
-    static boolean flag = false;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -15,47 +14,31 @@ public class Main {
             for (int j = 0; j < 9; j++) {
                 int su = sc.nextInt();
                 sudoku[i][j] = su;
-                if (su > 0) {
-                    check(i, j, su - 1);
-                } else emptyPoints.add(new Point(i, j));
+                if (su > 0)
+                    check[i][j][su - 1] = true;
+                else emptyPoints.add(new Point(i, j));
             }
         }
         solve();
     }
 
-    static void check (int x, int y, int v) {
-        sudoku[x][y] = v + 1;
-        check[0][x][v] = true;
-        check[1][y][v] = true;
-        check[2][(x / 3) * 3 + (y / 3)][v] = true;
-    }
-
-    static void reset (int x, int y, int v) {
-        sudoku[x][y] = 0;
-        check[0][x][v] = false;
-        check[1][y][v] = false;
-        check[2][(x / 3) * 3 + (y / 3)][v] = false;
-    }
-
-    static void solve () {
+    static boolean solve () {
         if (emptyPoints.isEmpty()) {
             print();
-            flag = true;
-            return;
+            return true;
         }
         Point p = emptyPoints.remove();
-        int temp = (p.x / 3) * 3 + (p.y / 3);
-        boolean result = true;
-        int count = 0;
         for (int i = 0; i < 9; i++) {
-            if (!check[0][p.x][i] && !check[1][p.y][i] && !check[2][temp][i]) {
-                check(p.x, p.y, i);
-                solve();
-                if (flag) return;
-                reset(p.x, p.y, i);
+            if (!check[p.x][p.y][i]) {
+                check[p.x][p.y][i] = true;
+                sudoku[p.x][p.y] = i + 1;
+                if (solve()) return true;
+                check[p.x][p.y][i] = false;
+                sudoku[p.x][p.y] = 0;
             }
         }
         emptyPoints.add(p);
+        return false;
     }
 
     static void print() {
