841979e4e6fac00109682b8d695226dcb8868587
lmjing <lmjing1234@gmail.com>
2018-08-13 21:22:31 +0900

    1201 TODO

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -9,34 +9,9 @@ public class Main {
         int m = sc.nextInt();
         int k = sc.nextInt();
 
-        System.out.println(solve(n, m, k));
-    }
+        // TODO : 어떻게 푸는지 알았거든? 내일 상기 시키면서 시도해보기.
+        if (n < m * k && n + 1 >= m + k) {
 
-    static StringBuilder solve (int n, int m, int k) {
-        if (n % 2 == 1 && m + k != n + 1)
-            return new StringBuilder("-1");
-        if (n % 2 == 0 && (m + k != n + 1) && !(m == n / 2 && k == n / 2))
-            return new StringBuilder("-1");
-        StringBuilder result = new StringBuilder();
-        int s = 1, e = n;
-
-        // 오름차순
-        while (s < m)
-            result.append(s++ + " ");
-
-        result.append(e-- + " ");
-        // 내림차순
-        int temp = s + (k - 1);
-        String des = "";
-        while (s < temp)
-            des = (s++) + " " + des;
-
-        result.append(des);
-
-        while (s <= e) {
-            result.append(e-- + " ");
-            if (s <= e) result.append(s++ + " ");
         }
-        return result;
     }
 }
