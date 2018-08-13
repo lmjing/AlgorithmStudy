8300c124918da0f88e41dd1f5575f19f0f812a34
lmjing <lmjing1234@gmail.com>
2018-08-13 20:26:08 +0900

    1201 1차시도 컴파일 에러

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,32 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        if (m + k > n) System.out.print(-1);
+        else {
+            int s = 1, e = n;
+            StringBuilder result = new StringBuilder();
+            // 오름차순
+            while (s < m)
+                result.append(s++ + " ");
+            result.append(e-- + " ");
+            // 내림차순
+            int temp = s + (m - 1);
+            String des = "";
+            while (s < temp)
+                des = (s++) + " " + des;
+
+            result.append(des);
+
+            while (s <= e) {
+                result.append(e-- + " ");
+                if (s <= e) result.append(s++ + " ");
+            }
+            System.out.print(result);
+        }
     }
 }
