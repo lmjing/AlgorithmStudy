e9b5d6729a7ca8d181e88b2a8899c752e760875f
lmjing <lmjing1234@gmail.com>
2018-08-13 21:04:19 +0900

    1201 4차시도 틀림

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -9,32 +9,34 @@ public class Main {
         int m = sc.nextInt();
         int k = sc.nextInt();
 
-        if (m + k != n + 1)
-            System.out.print(-1);
-        else {
-            int i = 1;
-            while (i < m)
-            
-            int s = 1, e = n;
+        System.out.println(solve(n, m, k));
+    }
 
-            // 오름차순
-            while (s < m)
-                result.append(s++ + " ");
+    static StringBuilder solve (int n, int m, int k) {
+        if (n % 2 == 1 && m + k != n + 1)
+            return new StringBuilder("-1");
+        if (n % 2 == 0 && (m + k != n + 1) && !(m == n / 2 && k == n / 2))
+            return new StringBuilder("-1");
+        StringBuilder result = new StringBuilder();
+        int s = 1, e = n;
 
+        // 오름차순
+        while (s < m)
+            result.append(s++ + " ");
+
+        result.append(e-- + " ");
+        // 내림차순
+        int temp = s + (k - 1);
+        String des = "";
+        while (s < temp)
+            des = (s++) + " " + des;
+
+        result.append(des);
+
+        while (s <= e) {
             result.append(e-- + " ");
-            // 내림차순
-            int temp = s + (k - 1);
-            String des = "";
-            while (s < temp)
-                des = (s++) + " " + des;
-
-            result.append(des);
-
-            while (s <= e) {
-                result.append(e-- + " ");
-                if (s <= e) result.append(s++ + " ");
-            }
-            System.out.print(result);
+            if (s <= e) result.append(s++ + " ");
         }
+        return result;
     }
 }
