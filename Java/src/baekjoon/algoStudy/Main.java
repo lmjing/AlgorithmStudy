2c7ced01cac38609574c8a2168fc7611bc0da829
lmjing <lmjing1234@gmail.com>
2018-08-16 19:18:19 +0900

    1780번 재채점해서 틀려서 다시 풀어서 정답

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -3,6 +3,39 @@ package baekjoon.algoStudy;
 import java.util.*;
 
 public class Main {
+    static int[][] map;
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = sc.nextInt() + 1;
+            }
+        }
+        for (int c : getCount(new int[]{0, 0}, n))
+            System.out.println(c);
+    }
+
+    static int[] getCount (int[] s, int n) {
+        int[] count = new int[3];
+        if (n == 1) {
+            count[map[s[0]][s[1]]]++;
+            return count;
+        }
+        int last = 3;
+        for (int i = s[0]; i < s[0] + n; i += n / 3) {
+
+            for (int j = s[1]; j < s[1] + n; j += n / 3) {
+                int[] result = getCount(new int[]{i, j}, n / 3);
+                for (int k = 0; k < 3; k++) {
+                    if (result[k] > 0) last = k;
+                    count[k] += result[k];
+                }
+            }
+        }
+        if (count[last] == 9 && count[0] + count[1] + count[2] == 9)
+            count[last] = 1;
+        return count;
     }
 }
