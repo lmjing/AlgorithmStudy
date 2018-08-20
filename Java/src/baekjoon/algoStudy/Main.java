fa1b532e31ab28849eada68e57e4d585e15cb4bd
lmjing <lmjing1234@gmail.com>
2018-08-21 00:25:17 +0900

    11005번 1차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,21 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] inputs = sc.nextLine().toCharArray();
+
+        int answer = 0;
+        int p = 1;
+        int i = inputs.length - 1;
+        while (i > -1) {
+            int octal = 0;
+            for (int pb = 1; i >= 0 && pb < 8; pb *= 2) {
+                octal += pb * (inputs[i] - '0');
+                i--;
+            }
+            answer += p * octal;
+            p *= 10;
+        }
+        System.out.println(answer);
     }
 }
