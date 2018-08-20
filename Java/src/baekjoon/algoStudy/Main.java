713f981fe4cf05243abfe4e80cafb6b06a4db521
lmjing <lmjing1234@gmail.com>
2018-08-20 18:18:12 +0900

    9613 코드 이동

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,24 +4,5 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int k = 0; k < t; k++) {
-            int n = sc.nextInt();
-            int[] inputs = new int[n];
-            for (int i = 0; i < n; i++) inputs[i] = sc.nextInt();
-
-            long sum = 0;
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = i + 1; j < n; j++)
-                    sum += gcd(inputs[i], inputs[j]);
-            }
-            System.out.println(sum);
-        }
-    }
-
-    static int gcd (int a, int b) {
-        // a < b 일 경우 a % b = a; 이므로 gcd(b, a);를 호출함
-        return b == 0 ? a : gcd(b, a % b);
     }
 }
--- Java/src/baekjoon/algoStudy/Math1/GCD.java
+++ Java/src/baekjoon/algoStudy/Math1/GCD.java
@@ -5,9 +5,7 @@ import java.util.*;
 public class GCD {
     static int gcd (int a, int b) {
         // a < b 일 경우 a % b = a; 이므로 gcd(b, a);를 호출함
-        int r = a % b;
-        if (r == 0) return b;
-        return gcd(b, r);
+        return b == 0 ? a : gcd(b, a % b);
     }
 
     // TIME : 원리 알고있는 상태에선 5분만에 구현
@@ -20,6 +18,23 @@ public class GCD {
 
             int gcd = gcd(a, b);
             System.out.println(a * (b / gcd));
+        }
+    }
+
+    public static void num9613() {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int k = 0; k < t; k++) {
+            int n = sc.nextInt();
+            int[] inputs = new int[n];
+            for (int i = 0; i < n; i++) inputs[i] = sc.nextInt();
+
+            long sum = 0;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = i + 1; j < n; j++)
+                    sum += gcd(inputs[i], inputs[j]);
+            }
+            System.out.println(sum);
         }
     }
 }
