8e8fc66d2dc86a97f5a3faa799b8d05019f58ac2
lmjing <lmjing1234@gmail.com>
2018-08-20 18:28:36 +0900

    num2609 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,16 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int gcd = gcd(a, b);
+        System.out.println(gcd);
+        System.out.println(a * (b / gcd));
+    }
+
+    static int gcd (int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
