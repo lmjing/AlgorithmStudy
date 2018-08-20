286cd1b500bde0b5d8b7694d07024e0df7f6d4cb
lmjing <lmjing1234@gmail.com>
2018-08-20 18:25:52 +0900

    num2609 코드 이동

--- Swift3/AlgorithmExercise/Math.swift
+++ Swift3/AlgorithmExercise/Math.swift
@@ -8,14 +8,17 @@
 
 import Foundation
 
-let input = readLine()!.split(separator: " ").map{ Int($0)! }
-
-func getGcd(_ a : Int, _ b : Int) -> Int {
-    return b == 0 ? a : getGcd(b, a % b)
+class Math {
+    func getGcd(_ a : Int, _ b : Int) -> Int {
+        return b == 0 ? a : getGcd(b, a % b)
+    }
+    
+    func num2609() {
+        let input = readLine()!.split(separator: " ").map{ Int($0)! }
+        let gcd = getGcd(input[0], input[1])
+        let lcm = input[0] * input[1] / gcd
+        
+        print(gcd)
+        print(lcm)
+    }
 }
-
-let gcd = getGcd(input[0], input[1])
-let lcm = input[0] * input[1] / gcd
-
-print(gcd)
-print(lcm)
