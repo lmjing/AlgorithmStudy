17c78b75ee718fafc42858ddfd1f05373b73847e
lmjing <lmjing1234@gmail.com>
2018-08-21 21:07:31 +0900

    1373 2차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,15 +5,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] inputs = sc.nextLine().toCharArray();
+        String input = sc.nextLine();
+//        char[] inputs = sc.nextLine().toCharArray();
 
         int answer = 0;
         int p = 1;
-        int i = inputs.length - 1;
+        int i = input.length() - 1;
         while (i > -1) {
             int octal = 0;
             for (int pb = 1; i >= 0 && pb < 8; pb *= 2) {
-                octal += pb * (inputs[i] - '0');
+                octal += pb * (input.charAt(i) - '0');
                 i--;
             }
             answer += p * octal;
