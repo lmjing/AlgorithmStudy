372e05c67470202520930982bc037d7fd14d1157
lmjing <lmjing1234@gmail.com>
2018-08-21 21:44:49 +0900

    1373 3차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,21 +5,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-//        char[] inputs = sc.nextLine().toCharArray();
+        char[] inputs = sc.nextLine().toCharArray();
 
-        int answer = 0;
-        int p = 1;
-        int i = input.length() - 1;
+        int i = inputs.length - 1;
+        char[] outputs = new char[(inputs.length - 1) / 3 + 1];
+        int j = outputs.length - 1;
+//        StringBuilder answer = new StringBuilder();
         while (i > -1) {
             int octal = 0;
             for (int pb = 1; i >= 0 && pb < 8; pb *= 2) {
-                octal += pb * (input.charAt(i) - '0');
+                octal += pb * (inputs[i] - '0');
                 i--;
             }
-            answer += p * octal;
-            p *= 10;
+            outputs[j--] = (char)(octal + 48);
         }
-        System.out.println(answer);
+        System.out.println(String.valueOf(outputs));
     }
 }
