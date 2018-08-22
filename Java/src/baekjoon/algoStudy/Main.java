f9e3622b0a00571b553eb8aa4d26a6b9c6f649be
lmjing <lmjing1234@gmail.com>
2018-08-22 20:23:00 +0900

    1929번 2차시도 만에 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[] check = new int[n + 1];
+
+        int i = 1;
+        while (++i * i <= n) {
+            if (check[i] == 0) {
+                if (i >= m)
+                    System.out.println(i);
+                for (int j = i * i; j <= n; j += i) {
+                    check[j] = -1;
+                }
+            }
+        }
+        while (i <= n) {
+            if (check[i] == 0 && i >= m)
+                System.out.println(i);
+            i++;
+        }
     }
 }
