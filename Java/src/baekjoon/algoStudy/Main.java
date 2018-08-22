e042c4a7e5cc888ad2ae4115b7e4ec7fd7e5e167
lmjing <lmjing1234@gmail.com>
2018-08-22 20:51:00 +0900

    6588번 1차시도 성공

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -5,24 +5,43 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-        int[] check = new int[n + 1];
+        int n = 1000000;
+        int[] prime = new int[1000001];
 
+        // 소수 체크
         int i = 1;
-        while (++i * i <= n) {
-            if (check[i] == 0) {
-                if (i >= m)
-                    System.out.println(i);
-                for (int j = i * i; j <= n; j += i) {
-                    check[j] = -1;
+        int lastPrime = -1;
+        boolean end = false;
+        while (++i <= n) {
+            if (prime[i] == 0) {
+                lastPrime = i;
+                if (end || i * i > n) {
+                    end = true;
+                    continue;
                 }
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = lastPrime;
+                }
+            } else
+                prime[i] = lastPrime;
+        }
+        prime[2] = prime[1] = -1;
+
+        int input = sc.nextInt();
+        while (input != 0) {
+            int b = prime[input];
+            int a = input - b;
+            while (b > 0 && prime[a] != 0) {
+                b = prime[b - 1];
+                a = input - b;
             }
+            if (b > 0)
+                System.out.println(input + " = " + a + " + " + b);
+            else
+                System.out.println("Goldbach's conjecture is wrong.");
+
+            input = sc.nextInt();
         }
-        while (i <= n) {
-            if (check[i] == 0 && i >= m)
-                System.out.println(i);
-            i++;
-        }
+
     }
 }
