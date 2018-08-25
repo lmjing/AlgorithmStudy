0b06e9fe731ab87ca643f9011c59b104a34718f6
lmjing <lmjing1234@gmail.com>
2018-08-25 11:55:30 +0900

    1158 진행 중 원형 링크드리스트 구현

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,79 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+         int n = sc.nextInt();
+         int m = sc.nextInt();
+
+        CircleLinkedList list = new CircleLinkedList();
+        for (int i = 1; i <= n; i++)
+            list.add(i);
+
+        list.printAll();
     }
+
+    static class CircleLinkedList {
+        Node front;
+        Node rear;
+        int size;
+
+        public CircleLinkedList () {
+            front = null;
+            rear = null;
+            size = 0;
+        }
+
+        public void add (int v) {
+            if (size == 0) {
+                rear = new Node(v);
+                rear.next = rear;
+                front = rear;
+            } else {
+                rear.next = new Node(v);
+                rear = rear.next;
+            }
+            size++;
+        }
+
+        public void printAll () {
+            Node cur = front;
+            while (cur.next != front) {
+                System.out.println(cur.vaule + " ");
+                cur = cur.next;
+            }
+            System.out.println(cur.vaule);
+        }
+
+        public int removeAt (Node node, int i) {
+            if (size == 0) return -1;
+
+            Node before = node;
+            while (--i > 0) {
+                before = node;
+                node = node.next;
+            }
+            return remove(before, node);
+        }
+
+        public int remove (Node before, Node node) {
+            size--;
+            int v = node.vaule;
+            if (size > 0) before.next = node.next;
+            node = null;
+            return v;
+        }
+
+        class Node {
+            int vaule;
+            Node next;
+
+            public Node (int v) {
+                vaule = v;
+                next = front;
+            }
+        }
+    }
+
+
 }
