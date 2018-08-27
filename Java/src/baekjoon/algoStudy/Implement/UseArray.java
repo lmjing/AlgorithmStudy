2c70a5c5e84b0e675bdcf41401178bb96beda0d2
lmjing <lmjing1234@gmail.com>
2018-08-27 15:48:18 +0900

    1184번 코드 이동

---
+++ Java/src/baekjoon/algoStudy/Implement/UseArray.java
@@ -0,0 +1,66 @@
+package baekjoon.algoStudy.Implement;
+
+import java.util.*;
+
+public class UseArray {
+    // 구현 문제
+    public class Main {
+        int[][] map;
+        int n;
+        int[] fx = { 1, 1, -1, -1};
+        int[] fy = { -1, 1, -1, 1};
+        int[] dx = { 0, 0, -1, -1};
+        int[] dy = { 0, 1, 0, 1};
+
+        public void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            n = sc.nextInt();
+            map = new int[n + 1][n + 1];
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    map[i][j] = sc.nextInt();
+                }
+            }
+
+            int allCount = 0;
+            // 교차점 순회
+            for (int i = 2; i <= n; i++) {
+                for (int j = 1; j < n; j++) {
+                    allCount += pointCount(i, j);
+                }
+            }
+            System.out.println(allCount);
+        }
+
+        private int pointCount(int x, int y) {
+            int same = 0;
+            for (int dir = 0; dir < 2; dir++) {
+                ArrayList<Long> left = getRevenue(x + dx[dir], y + dy[dir], dir);
+                int rd = 3 - dir;
+                ArrayList<Long> right = getRevenue(x + dx[rd], y + dy[rd], rd);
+
+                for (long l : left) {
+                    for (long r : right) {
+                        if (l == r) same++;
+                    }
+                }
+            }
+
+            return same;
+        }
+
+        private ArrayList<Long> getRevenue(int x, int y, int dir) {
+            long[][] revMap = new long[n + 1][n + 1];
+            ArrayList<Long> revenue = new ArrayList<>();
+            for (int i = x; fx[dir] > 0 ? i <= n : i > 0; i += fx[dir]) {
+                int horSum = 0;
+                for (int j = y; fy[dir] > 0 ? j <= n : j > 0; j += fy[dir]) {
+                    horSum += map[i][j];
+                    revMap[i][j] = revMap[i - fx[dir]][j] + horSum;
+                    revenue.add(revMap[i][j]);
+                }
+            }
+            return revenue;
+        }
+    }
+}
