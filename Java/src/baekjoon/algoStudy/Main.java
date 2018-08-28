89156296882e2d078798e12e6f32d315f4804b6c
lmjing <lmjing1234@gmail.com>
2018-08-28 12:39:44 +0900

    3045 이중연결 리스트 구현

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,66 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        DoubleLinkedList list = new DoubleLinkedList();
+
+        for (int i = 1; i <= n; i++)
+            list.addLast(i);
+
+        list.printAllBack();
+    }
+
+    public static class DoubleLinkedList {
+        Node front;
+        Node rear;
+        int size;
+
+        public DoubleLinkedList () {
+            front = null;
+            rear = null;
+        }
+
+        public void addLast (int v) {
+            Node newNode = new Node(v, rear);
+            if (size == 0) {
+                rear = newNode;
+                front = newNode;
+            } else {
+                rear.next = newNode;
+                rear = newNode;
+            }
+            size++;
+        }
+
+        public void printAll () {
+            Node cur = front;
+            while (cur != null) {
+                System.out.println(cur.vaule);
+                cur = cur.next;
+            }
+        }
+
+        public void printAllBack () {
+            Node cur = rear;
+            while (cur != null) {
+                System.out.println(cur.vaule);
+                cur = cur.before;
+            }
+        }
+
+        class Node {
+            int vaule;
+            Node before;
+            public Node next;
+
+            public Node (int v, Node b) {
+                vaule = v;
+                before = b;
+                next = null;
+            }
+        }
     }
 }
