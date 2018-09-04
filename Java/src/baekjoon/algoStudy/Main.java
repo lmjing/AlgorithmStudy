44a784e235bd7e03e5df29886c4e69073cf4e940
lmjing <lmjing1234@gmail.com>
2018-09-04 15:21:53 +0900

    1977 1차시도 실패

--- Java/src/baekjoon/algoStudy/Main.java
+++ Java/src/baekjoon/algoStudy/Main.java
@@ -4,5 +4,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int min = (int)Math.ceil(Math.sqrt(m));
+        int sum = 0;
+        for (int i = min; i <= Math.floor(Math.sqrt(n)); i++) {
+            sum += Math.pow(i, 2);
+        }
+
+        if (sum > 0) {
+            System.out.println((int)Math.pow(min, 2));
+            System.out.println(sum);
+        } else System.out.println(-1);
+
     }
 }
