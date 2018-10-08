6f778c83e90137915e768cac25d91815a5f2e394
lmjing <lmjing1234@gmail.com>
2018-10-08 20:31:08 +0900

    14588 플로이드 알고리즘 보고 해결

--- Java/src/baekjoon/individual/CodingTestPractice/LinePlus.java
+++ Java/src/baekjoon/individual/CodingTestPractice/LinePlus.java
@@ -4,21 +4,28 @@ import java.util.*;
 
 public class LinePlus {
     static int[][] lines; // 입력된 라인 시작 - 끝 좌표
-    static int[][] friends;
+    static int[][] dist; // 친구 관계 수
 
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         lines = new int[n + 1][2];
-        friends = new int[n + 1][n + 1];
+        dist = new int[n + 1][n + 1];
 
         for (int i = 1; i <= n; i++) {
             lines[i][0] = sc.nextInt();
             lines[i][1] = sc.nextInt();
 
             for (int j = 1; j < i; j++) {
-                if (!(lines[i][1] < lines[j][0] || lines[j][1] < lines[i][0])) {
-                    friends[i][j] = friends[j][i] = 1;
+                dist[i][j] = dist[j][i] = !(lines[i][1] < lines[j][0] || lines[j][1] < lines[i][0]) ? 1 : n;
+            }
+        }
+
+        for (int k = 1; k <= n; k++) { // 거쳐가는 정점
+            for (int i = 1; i <= n; i++) { // 출발하는 정점
+                for (int j = 1; j <= n; j++) { // 도착하는 정점
+                    if (dist[i][j] > dist[i][k] + dist[k][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
                 }
             }
         }
@@ -27,53 +34,7 @@ public class LinePlus {
         for (int i = 0; i < q; i++) {
             int s = sc.nextInt();
             int e = sc.nextInt();
-
-            boolean[] visited = new boolean[n + 1];
-            visited[0] = visited[s] = true;
-            System.out.println(getFriends(s, e, visited, 0));
+            System.out.println(dist[s][e] == n ? -1 : dist[s][e]);
         }
     }
-
-    static int getFriends (int s, int e, boolean[] visited, int vCnt) {
-        if (friends[s][e] != 0) {
-            return friends[s][e] + (friends[s][e] == -1 ? 0 : vCnt);
-        }
-
-        int min = visited.length;
-        for (int i = 1; i < friends.length; i++) {
-            if (!visited[i] && friends[s][i] == 1) {
-                visited[i] = true;
-                int fCnt = getFriends(i, e, visited, vCnt + 1);
-                if (min > fCnt && fCnt != -1) min = fCnt;
-                visited[i] = false;
-            }
-        }
-
-        if (vCnt == 0)
-            friends[s][e] = friends[e][s] = min;
-        if (min == visited.length) { // 연결 고리 없는 경우
-            min = -1;
-        }
-
-        return min;
-    }
-
-//    int getFriends (int s, int e, boolean[] visited) {
-//        if (s == e) return 0;
-//        //if () // 다 방문했으면 리턴 -1
-//        if (friends[s][e] > 0) return friends[s][e];
-//
-//        int min = friends.length;
-//        int sx = lines[s][0], sy = lines[s][1];
-//        for (int i = 1; i <= friends.length; i++) {
-//            if (i == s) continue; // 자신은 건너 뜀
-//
-//            if ((lines[i][0] >= sx && lines[i][0] <= sy) || (lines[i][1] >= sx && lines[i][1] <= sy)) {
-//                friends[s][i] = friends[i][s] = 1;
-//                visited[i] = true;
-////                int f = 1 + getFriends(i, e, )
-//            }
-//        }
-//
-//    }
 }
